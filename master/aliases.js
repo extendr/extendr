dc8e57de4d2717344cb221832cbb03580f009995
Travis CI User <travis@example.org>
2020-07-27 19:07:12 +0000

    ghp-upload script

--- master/aliases.js
+++
@@ -1,8 +0,0 @@
-var ALIASES = {};
-ALIASES["extendr_api"] = {};
-ALIASES["extendr_macros"] = {};
-ALIASES["libR_sys"] = {};
-ALIASES["proc_macro2"] = {};
-ALIASES["quote"] = {};
-ALIASES["syn"] = {};
-ALIASES["unicode_xid"] = {};
