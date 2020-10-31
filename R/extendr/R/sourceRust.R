54734baf9462e6147952937566ffa42ebb90875e
andy-thomason <andy@andythomason.com>
2020-10-31 16:14:13 +0000

    Updadate sourceRust.

--- R/extendr/R/sourceRust.R
+++ R/extendr/R/sourceRust.R
@@ -1,2 +1,15 @@
-sourceRust <-
-function() {}
+sourceRust <- function(
+    file = "",
+    code = NULL,
+    env = globalenv(),
+    embeddedR = TRUE,
+    rebuild = FALSE,
+    cacheDir = getOption("extendr.cache.dir", tempdir()),
+    cleanupCacheDir = FALSE,
+    showOutput = verbose,
+    verbose = getOption("verbose"),
+    dryRun = FALSE,
+    windowsDebugDLL = FALSE
+) {
+
+}
