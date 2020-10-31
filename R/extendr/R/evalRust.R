209b36d795071a7de2f40d84d5236999a81a5651
andy-thomason <andy@andythomason.com>
2020-10-31 16:11:10 +0000

    Default params for evalRust.

--- R/extendr/R/evalRust.R
+++ R/extendr/R/evalRust.R
@@ -1,4 +1,13 @@
 
-evalRust <- function() {
+evalRust <- function(
+    code,
+    depends = character(),
+    plugins = character(),
+    includes = character(),
+    rebuild = FALSE,
+    cacheDir = getOption("extendr.cache.dir", tempdir()),
+    showOutput = verbose,
+    verbose = getOption( "verbose" )
+) {
 
 }
