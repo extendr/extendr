bdd970c65239cd7125dfc1325111326f64971a93
andy-thomason <andy@andythomason.com>
2020-11-24 13:16:28 +0000

    bump to 0.1.10 using bump.sh

--- publish.sh
+++ publish.sh
@@ -2,12 +2,18 @@
 
 set -euo pipefail
 
+echo pushing with tags to github
 git push --tags
 
+echo publishing extendr-macros
 pushd extendr-macros
 cargo publish
 popd
 
+echo waiting for crates.io to catch up
+sleep 20
+
+echo publishing extendr-api
 pushd extendr-api
 cargo publish
 popd
