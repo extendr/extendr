62c1d9350c6167f1a4784cd869bcef3ba4dc75c1
clauswilke <clauswilke@users.noreply.github.com>
2020-12-10 17:54:24 +0000

    deploy: 2a19999bf763e5a07bb47747a514a34814afe870

---
+++ extendr_engine/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["end_r","Close down the R interpreter. Note you won't be able to Restart it, so use with care or not at all."],["start_r",""]]});
--- source-files.js
+++ source-files.js
@@ -1,4 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["extendr_api"] = {"name":"","files":["args.rs","engine.rs","lib.rs","logical.rs","rmacros.rs","robj.rs","wrapper.rs"]};
+sourcesIndex["extendr_api"] = {"name":"","files":["args.rs","error.rs","lib.rs","logical.rs","rmacros.rs","robj.rs","wrapper.rs"]};
+sourcesIndex["extendr_engine"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["extendr_macros"] = {"name":"","files":["lib.rs","output_r.rs"]};
 createSourceSidebar();
