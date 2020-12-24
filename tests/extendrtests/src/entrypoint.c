5dc131468ff70bb299a53f0b23a353ec72c8f79b
Claus Wilke <wilke@austin.utexas.edu>
2020-12-24 11:42:28 -0600

    going all out

--- tests/extendrtests/src/entrypoint.c
+++ tests/extendrtests/src/entrypoint.c
@@ -26,7 +26,7 @@ static const R_CallMethodDef CallEntries[] = {
   {NULL, NULL, 0}
 };
 
-void R_init_extendrtests(DllInfo *dll) {
+void R_init_extendrtests_decoy(DllInfo *dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
   R_forceSymbols(dll, TRUE);
