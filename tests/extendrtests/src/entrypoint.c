b4ee9f71125f95ed9eac001570f86c0865eb7217
Claus Wilke <wilke@austin.utexas.edu>
2020-12-23 23:19:25 -0600

    force symbols

--- tests/extendrtests/src/entrypoint.c
+++ tests/extendrtests/src/entrypoint.c
@@ -29,4 +29,5 @@ static const R_CallMethodDef CallEntries[] = {
 void R_init_extendrtests(DllInfo *dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
+  R_forceSymbols(dll, TRUE);
 }
