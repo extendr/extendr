ddf8ee57f50ad7287ab5b236905623ab7ea67b65
andy-thomason <andy-thomason@users.noreply.github.com>
2020-12-29 21:48:10 +0000

    deploy: 1b641886b9cf32a4a02f40068190a6dc3e65613c

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["extendr_api"] = {"name":"","files":["args.rs","error.rs","lib.rs","logical.rs","rmacros.rs","robj.rs","wrapper.rs"]};
+sourcesIndex["extendr_api"] = {"name":"","dirs":[{"name":"robj","files":["from_robj.rs","into_robj.rs","iter.rs","mod.rs","rinternals.rs","symbols.rs"]}],"files":["error.rs","lang.rs","lib.rs","logical.rs","rmacros.rs","wrapper.rs"]};
 sourcesIndex["extendr_engine"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["extendr_macros"] = {"name":"","files":["lib.rs","output_r.rs"]};
 createSourceSidebar();
