5e9ddc0b6e84be1be68f62e7bc68f3dd00cd6cfe
andy-thomason <andy@andythomason.com>
2021-01-22 19:24:07 +0000

    Another iteration of the wrappers.

--- tests/extendrtests/src/rust/src/lib.rs
+++ tests/extendrtests/src/rust/src/lib.rs
@@ -1,5 +1,6 @@
 use extendr_api::*;
 
+/// Say hello
 #[extendr]
 fn hello_world() -> &'static str {
     "Hello world!"
@@ -17,30 +18,42 @@ fn double_scalar(x: f64) -> f64 { x }
 #[extendr]
 fn int_scalar(x: i32) -> i32 { x }
 
+/// convert a bool scalar to itself
+/// @param x a number
 #[extendr]
 fn bool_scalar(x: bool) -> bool { x }
+
+/// convert a string to itself
+/// @param x a number
 #[extendr]
 fn char_scalar(x: String) -> String { x }
 
+/// Class for testing
 #[derive(Default, Debug)]
 struct MyClass {
     a: i32,
 }
 
+/// Class for testing
 #[extendr]
 impl MyClass {
+    /// Method for making new object.
     fn new() -> Self {
         Self { a: 0 }
     }
     
+    /// Method for setting stuff.
+    /// @param x a number
     fn set_a(& mut self, x: i32) {
         self.a = x;
     }
     
+    /// Method for getting stuff.
     fn a(&self) -> i32 {
         self.a
     }
     
+    /// Method for getting one's self.
     fn me(&self) -> &Self {
         self
     }
