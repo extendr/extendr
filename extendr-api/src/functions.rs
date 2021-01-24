37791965c4c3fe99b048a5225ce4e57bbcfa4e96
Claus Wilke <wilke@austin.utexas.edu>
2021-01-24 17:26:24 -0600

    Remove extendr-engine call from non-testing code. (#150)
    * Remove extendr-engine call from non-testing code. Fixes #144.
    
    * simplify patch crates
    
    * simplify patch crates
    
    * cfg(test) not needed

--- extendr-api/src/functions.rs
+++ extendr-api/src/functions.rs
@@ -376,17 +376,3 @@ pub fn blank_scalar_string() -> Robj {
 pub fn na_str() -> &'static str {
     unsafe { std::str::from_utf8_unchecked(&[b'N', b'A']) }
 }
-
-/// Extendr test harness.
-///
-/// See also [test!]
-/// ```
-/// extendr_api::test(|| {
-///   Ok(())
-/// })
-/// ```
-pub fn test<F: FnOnce() -> Result<()>>(f: F) {
-    extendr_engine::start_r();
-
-    f().unwrap();
-}
