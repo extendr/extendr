7d3a12040fb15d154b14c21afd6b40d05a8cafbd
andy-thomason <andy-thomason@users.noreply.github.com>
2021-01-28 12:24:33 +0000

    deploy: c0f77abff8a7b92a2c124f4240fab0d6c413ddaa

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["extendr_api"] = {"name":"","dirs":[{"name":"robj","files":["from_robj.rs","into_robj.rs","iter.rs","mod.rs","operators.rs","rinternals.rs"]}],"files":["error.rs","functions.rs","lang.rs","lib.rs","logical.rs","matrix.rs","metadata.rs","rmacros.rs","robj_ndarray.rs","thread_safety.rs","wrapper.rs"]};
+sourcesIndex["extendr_api"] = {"name":"","dirs":[{"name":"robj","files":["from_robj.rs","into_robj.rs","mod.rs","operators.rs","rinternals.rs"]}],"files":["error.rs","functions.rs","iter.rs","lang_macros.rs","lib.rs","logical.rs","matrix.rs","metadata.rs","prelude.rs","rmacros.rs","robj_ndarray.rs","thread_safety.rs","wrapper.rs"]};
 sourcesIndex["extendr_engine"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["extendr_macros"] = {"name":"","files":["lib.rs","output_r.rs"]};
 createSourceSidebar();
