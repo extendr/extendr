eb2c016fd0a8ea647a683e599ac63a7a7599b685
andy-thomason <andy@andythomason.com>
2021-02-01 21:19:13 +0000

    Add nullable NA example.

--- extendr-api/src/wrapper.rs
+++ extendr-api/src/wrapper.rs
@@ -160,12 +160,24 @@ pub struct Primitive<'a>(pub &'a str);
 /// ```
 /// use extendr_api::prelude::*;
 /// test! {
+///     use extendr_api::wrapper::Nullable::*;
+///
+///     // Plain integer.
 ///     let s1 = r!(1);
 ///     let n1 = <Nullable<i32>>::from_robj(&s1)?;
-///     assert_eq!(n1, Nullable::NotNull(1));
+///     assert_eq!(n1, NotNull(1));
+///
+///     // NA integer - error.
+///     let sna = r!(NA_INTEGER);
+///     assert_eq!(<Nullable<i32>>::from_robj(&sna).is_err(), true);
+///
+///     // NA integer - option gives none.
+///     assert_eq!(<Nullable<Option<i32>>>::from_robj(&sna)?, NotNull(None));
+///
+///     // NULL object.
 ///     let snull = r!(NULL);
 ///     let nnull = <Nullable<i32>>::from_robj(&snull)?;
-///     assert_eq!(nnull, Nullable::Null);
+///     assert_eq!(nnull, Null);
 ///
 ///     assert_eq!(r!(Nullable::<i32>::Null), r!(NULL));
 ///     assert_eq!(r!(Nullable::<i32>::NotNull(1)), r!(1));
