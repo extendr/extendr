ed04dea50945d32c90224dd7505f32d61e48f5d7
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-02-02 12:58:58 +0200

    Coments & cleanup

--- ci-cargo.ps1
+++ ci-cargo.ps1
@@ -1,4 +1,5 @@
 function ci-cargo {
+
     param(
         [Parameter(Position = 0, ValueFromRemainingArguments)]
         [String[]]
@@ -6,6 +7,8 @@ function ci-cargo {
         [String]
         $ActionName
     )
+
+
     try {    
         echo "::group::$ActionName"
         echo "Running cargo $CargoArgs"
@@ -25,5 +28,40 @@ function ci-cargo {
     finally {
         echo "::endgroup::"
     }
+
+    <#
+        .SYNOPSIS
+        Runs cargo with specified args, adapting error handling and output to CI.
+
+        .DESCRIPTION
+        Runs cargo in a `try` block, catches exceptions and non-zero exit codes.
+        Explicitly logs the beginning and the end of cargo execution, as well as the error message.
+
+        .PARAMETER CargoArgs
+        Arguments passed to cargo, as-is.
+        Note that `--` separator handled by powershell itself,
+        so it should be wrapped in quotes `'--'` and passed as string.
+
+        .PARAMETER ActionName
+        Optional string that is used to format logs and error messages.
+
+        .INPUTS
+        None. You cannot pipe objects.
+
+        .OUTPUTS
+        No explicit output.
+
+        .EXAMPLE
+        PS> ci-cargo --version
+
+        .EXAMPLE
+        PS> ci-cargo -ActioName "Build"  build
+
+        .EXAMPLE
+        PS> ci-cargo +stable-x86_64-pc-windows-gnu test --features tests-all --target i686-pc-windows-gnu '--' --nocapture -ActionName "Called from documentation"
+
+        .LINK
+        Used by: https://github.com/extendr/extendr
+    #>
     
 }
