3c39e81d605ed4270b132bfc8d85eb0ee2c11392
andy-thomason <andy@andythomason.com>
2021-02-17 22:39:07 +0000

    Format

--- extendr-api/src/lib.rs
+++ extendr-api/src/lib.rs
@@ -373,30 +373,30 @@ impl IsNA for &str {
 /// Type of R objects used by [Robj::rtype].
 #[derive(Debug, PartialEq)]
 pub enum RType {
-    Null, // NILSXP
-    Symbol, // SYMSXP
-    Pairlist, // LISTSXP
-    Function, // CLOSXP
-    Enviroment, // ENVSXP
-    Promise, // PROMSXP
-    Language, // LANGSXP
-    Special, // SPECIALSXP
-    Builtin, // BUILTINSXP
-    Character, // CHARSXP
-    Logical, // LGLSXP
-    Integer, // INTSXP
-    Real, // REALSXP
-    Complex, // CPLXSXP
-    String, // STRSXP
-    Dot, // DOTSXP
-    Any, // ANYSXP
-    List, // VECSXP
-    Expression, // EXPRSXP
-    Bytecode, // BCODESXP
+    Null,        // NILSXP
+    Symbol,      // SYMSXP
+    Pairlist,    // LISTSXP
+    Function,    // CLOSXP
+    Enviroment,  // ENVSXP
+    Promise,     // PROMSXP
+    Language,    // LANGSXP
+    Special,     // SPECIALSXP
+    Builtin,     // BUILTINSXP
+    Character,   // CHARSXP
+    Logical,     // LGLSXP
+    Integer,     // INTSXP
+    Real,        // REALSXP
+    Complex,     // CPLXSXP
+    String,      // STRSXP
+    Dot,         // DOTSXP
+    Any,         // ANYSXP
+    List,        // VECSXP
+    Expression,  // EXPRSXP
+    Bytecode,    // BCODESXP
     ExternalPtr, // EXTPTRSXP
-    WeakRef, // WEAKREFSXP
-    Raw, // RAWSXP
-    S4, // S4SXP
+    WeakRef,     // WEAKREFSXP
+    Raw,         // RAWSXP
+    S4,          // S4SXP
     Unknown,
 }
 
