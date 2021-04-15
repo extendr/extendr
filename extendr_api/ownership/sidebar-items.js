3609afc2243792001c2d8a7f01c89b8c4b657b2b
andy-thomason <andy-thomason@users.noreply.github.com>
2021-04-15 21:53:15 +0000

    deploy: 109bb85cccb31fb3a220348ecbd1100daf9c3bcb

--- extendr_api/ownership/sidebar-items.js
+++ extendr_api/ownership/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["EXTRA_PRESERVATION_SIZE",""],["INITIAL_PRESERVATION_SIZE",""]],"fn":[["protect",""],["unprotect",""]]});
+initSidebarItems({"constant":[["EXTRA_PRESERVATION_SIZE",""],["INITIAL_PRESERVATION_SIZE",""]]});
