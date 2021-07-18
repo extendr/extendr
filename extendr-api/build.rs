1ced3a97630b7b07ae00d26dd8085df981f36100
Hiroaki Yutani <yutani.ini@gmail.com>
2021-07-18 16:37:27 +0900

    Add a note

--- extendr-api/build.rs
+++ extendr-api/build.rs
@@ -1,6 +1,12 @@
 use std::env;
 
 fn main() {
+    // The R version information is needed to handle the API differences
+    // between versions. `These DEP_R_R_VERSION_*` are provided by libR-sys
+    // (for more details, please refer to libR-sys's `build.rs`).
+    // The current approach is to add a config flag per feature. When there are
+    // too many features, we might need to consider switching to per-version
+    // config flags (e.g. `r410`).
     let major = env::var("DEP_R_R_VERSION_MAJOR").unwrap();
     let minor = env::var("DEP_R_R_VERSION_MINOR").unwrap();
     // let patch = env::var("DEP_R_R_VERSION_PATCH").unwrap();
