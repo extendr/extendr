edefff2c1a3bccd9c4a2ffadd7e44e07bcbb42c9
andy-thomason <andy-thomason@users.noreply.github.com>
2021-07-28 10:28:00 +0000

    deploy: 42d168bee850bbc3fdb3b0737832fe0f3679451d

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["extendr_api"] = {"name":"","dirs":[{"name":"robj","files":["from_robj.rs","into_robj.rs","mod.rs","operators.rs","rinternals.rs","try_from_robj.rs"]},{"name":"wrapper","files":["character.rs","environment.rs","expr.rs","function.rs","lang.rs","list.rs","matrix.rs","mod.rs","nullable.rs","pairlist.rs","primitive.rs","promise.rs","raw.rs","symbol.rs"]}],"files":["error.rs","functions.rs","iter.rs","lang_macros.rs","lib.rs","logical.rs","metadata.rs","ownership.rs","prelude.rs","rmacros.rs","robj_ndarray.rs","thread_safety.rs"]};
 sourcesIndex["extendr_engine"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["extendr_macros"] = {"name":"","files":["R.rs","extendr_function.rs","extendr_impl.rs","extendr_module.rs","lib.rs","pairlist.rs","pairs.rs","wrappers.rs"]};
+sourcesIndex["extendr_macros"] = {"name":"","files":["R.rs","call.rs","extendr_function.rs","extendr_impl.rs","extendr_module.rs","lib.rs","pairlist.rs","pairs.rs","wrappers.rs"]};
 createSourceSidebar();
