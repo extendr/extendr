5ab3f6076021104043641bd7e9e91417f30c80f5
Hiroaki Yutani <yutani.ini@gmail.com>
2021-08-03 01:56:36 +0900

    Check the limits and the rounding of to-int conversions (#243)
    * Check the limits and the rounding of to-int conversions
    
    * Try as_real() first
    
    * Improve error message
    
    * Improve description
    
    * Improve comment [skip ci]

--- extendr-api/src/robj/try_from_robj.rs
+++ extendr-api/src/robj/try_from_robj.rs
@@ -2,54 +2,100 @@
 
 use super::*;
 
-macro_rules! impl_try_from_scalar {
-    ($t: ty) => {
+macro_rules! impl_try_from_scalar_integer {
+    ($t:ty) => {
         impl TryFrom<Robj> for $t {
             type Error = Error;
 
             fn try_from(robj: Robj) -> Result<Self> {
-                if let Some(v) = robj.as_integer_slice() {
-                    match v.len() {
-                        0 => Err(Error::ExpectedNonZeroLength(robj)),
-                        1 => {
-                            if !v[0].is_na() {
-                                Ok(v[0] as Self)
-                            } else {
-                                Err(Error::MustNotBeNA(robj))
-                            }
-                        }
-                        _ => Err(Error::ExpectedScalar(robj)),
-                    }
-                } else if let Some(v) = robj.as_real_slice() {
-                    match v.len() {
-                        0 => Err(Error::ExpectedNonZeroLength(robj)),
-                        1 => {
-                            if !v[0].is_na() {
-                                Ok(v[0] as Self)
-                            } else {
-                                Err(Error::MustNotBeNA(robj))
-                            }
-                        }
-                        _ => Err(Error::ExpectedScalar(robj)),
-                    }
-                } else {
-                    Err(Error::ExpectedNumeric(robj))
+                // Check if the value is a scalar
+                match robj.len() {
+                    0 => return Err(Error::ExpectedNonZeroLength(robj)),
+                    1 => {}
+                    _ => return Err(Error::ExpectedScalar(robj)),
+                };
+
+                // Check if the value is not a missing value
+                if robj.is_na() {
+                    return Err(Error::MustNotBeNA(robj));
                 }
+
+                // If the conversion is int-to-int, check the limits. This
+                // needs to be done by `TryFrom` because the conversion by `as`
+                // is problematic when converting a negative value to unsigned
+                // integer types (e.g. `-1i32 as u8` becomes 255).
+                if let Some(v) = robj.as_integer() {
+                    if let Ok(v) = Self::try_from(v) {
+                        return Ok(v);
+                    } else {
+                        return Err(Error::OutOfLimits(robj));
+                    }
+                }
+
+                // If the conversion is float-to-int, check if the value is
+                // integer-like (i.e., an integer, or a float representing a
+                // whole number). This needs to be down with `as`, as no
+                // `TryFrom` is implemented for float types. `FloatToInt` trait
+                // might eventually become available in future, though.
+                if let Some(v) = robj.as_real() {
+                    let result = v as Self;
+                    if ((result as f64 - v).abs() < f64::EPSILON) {
+                        return Ok(result);
+                    } else {
+                        return Err(Error::ExpectedWholeNumber(robj));
+                    }
+                }
+
+                Err(Error::ExpectedNumeric(robj))
             }
         }
     };
 }
 
-impl_try_from_scalar!(u8);
-impl_try_from_scalar!(u16);
-impl_try_from_scalar!(u32);
-impl_try_from_scalar!(u64);
-impl_try_from_scalar!(i8);
-impl_try_from_scalar!(i16);
-impl_try_from_scalar!(i32);
-impl_try_from_scalar!(i64);
-impl_try_from_scalar!(f32);
-impl_try_from_scalar!(f64);
+macro_rules! impl_try_from_scalar_real {
+    ($t:ty) => {
+        impl TryFrom<Robj> for $t {
+            type Error = Error;
+
+            fn try_from(robj: Robj) -> Result<Self> {
+                // Check if the value is a scalar
+                match robj.len() {
+                    0 => return Err(Error::ExpectedNonZeroLength(robj)),
+                    1 => {}
+                    _ => return Err(Error::ExpectedScalar(robj)),
+                };
+
+                // Check if the value is not a missing value
+                if robj.is_na() {
+                    return Err(Error::MustNotBeNA(robj));
+                }
+
+                // <Robj>::as_xxx() methods can work only when the underlying
+                // SEXP is the corresponding type, so we cannot use as_real()
+                // directly on INTSXP.
+                if let Some(v) = robj.as_real() {
+                    return Ok(v as Self);
+                }
+                if let Some(v) = robj.as_integer() {
+                    return Ok(v as Self);
+                }
+
+                Err(Error::ExpectedNumeric(robj))
+            }
+        }
+    };
+}
+
+impl_try_from_scalar_integer!(u8);
+impl_try_from_scalar_integer!(u16);
+impl_try_from_scalar_integer!(u32);
+impl_try_from_scalar_integer!(u64);
+impl_try_from_scalar_integer!(i8);
+impl_try_from_scalar_integer!(i16);
+impl_try_from_scalar_integer!(i32);
+impl_try_from_scalar_integer!(i64);
+impl_try_from_scalar_real!(f32);
+impl_try_from_scalar_real!(f64);
 
 impl TryFrom<Robj> for Bool {
     type Error = Error;
