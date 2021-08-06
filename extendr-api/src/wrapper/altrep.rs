156092b66e24120538fc193569f71372aa72ea64
andy-thomason <andy@andythomason.com>
2021-08-06 21:12:12 +0100

    Comment all the implementation functions.

--- extendr-api/src/wrapper/altrep.rs
+++ extendr-api/src/wrapper/altrep.rs
@@ -6,31 +6,49 @@ pub struct Altrep {
 }
 
 /// Rust trait for implementing ALTREP.
+/// Implement one or more of these methods to generate an Altrep class.
+/// Mechanism TBD.
 trait AltrepImpl {
-    fn unserialize_ex(_class: Robj, _state: Robj, _attr: Robj, _objf: i32, _levs: i32) -> Robj {
-        ().into()
+    /// Constructor that is called when loading an Altrep object from a file.
+    fn unserialize_ex(class: Robj, state: Robj, attributes: Robj, obj_flags: i32, levels: i32) -> Robj {
+        let res = Self::unserialize(class, state);
+        if !res.is_null() {
+            unsafe {
+                let val = res.get();
+                SET_ATTRIB(val, attributes.get());
+                SET_OBJECT(val, obj_flags);
+                SETLEVELS(val, levels);
+            }
+        }
+        res
     }
 
+    /// Simplified constructor that is called when loading an Altrep object from a file.
     fn unserialize(_class: Robj, _state: Robj) -> Robj {
         ().into()
     }
 
+    /// Fetch the state of this object when writing to a file.
     fn serialized_state(&self) -> Robj {
         ().into()
     }
 
+    /// Duplicate this object, possibly duplicating attributes.
     fn duplicate_ex(&self, _deep: bool) -> Robj {
         ().into()
     }
 
+    /// Duplicate this object. Called by Rf_duplicate.
     fn duplicate(&self, _deep: bool) -> Robj {
         ().into()
     }
 
+    /// Coerce this object into some other type, if possible.
     fn coerce(&self, _ty: RType) -> Robj {
         ().into()
     }
 
+    /// Print the text for .Internal(inspect(obj))
     fn inspect(
         &self,
         _pre: i32,
@@ -43,114 +61,182 @@ trait AltrepImpl {
 }
 
 trait AltvecImpl: AltrepImpl {
+    /// Get the virtual length of the vector.
+    /// For example for a compact range, return end - start + 1.
     fn length(&self) -> usize {
         0
     }
+
+    /// Get the data pointer for this vector, possibly expanding the
+    /// compact representation into a full R vector.
+    /// We may move this into AltIntegerImpl etc. and use a slice.
     fn dataptr(&self, _writeable: bool) -> *mut u8 {
         std::ptr::null_mut()
     }
+
+    /// Get the data pointer for this vector, returning NULL
+    /// if the object is 
+    /// We may move this into AltIntegerImpl etc. and use a slice.
     fn dataptr_or_null(&self) -> *const u8 {
         std::ptr::null()
     }
+
+    /// Implement subsetting (eg. x[10:19]) for this Altrep vector.
     fn extract_subset(&self, _indx: Robj, _call: Robj) -> Robj {
         ().into()
     }
 }
 
 trait AltIntegerImpl: AltvecImpl {
+    /// Get a single element from this vector.
     fn elt(&self, _index: usize) -> i32 {
         0
     }
+
+    /// Get a multiple elements from this vector.
     fn get_region(&self, _index: usize, _data: &mut [i32]) -> usize {
         0
     }
+
+    /// Return true if this vector is sorted.
     fn is_sorted(&self) -> bool {
         false
     }
+
+    /// Return true if this vector does not contain NAs.
     fn no_na(&self) -> bool {
         false
     }
+
+    /// Return the sum of the elements in this vector.
+    /// If remove_nas is true, skip and NA values.
     fn sum(&self, _remove_nas: bool) -> Robj {
         ().into()
     }
+
+    /// Return the minimum of the elements in this vector.
+    /// If remove_nas is true, skip and NA values.
     fn min(&self, _remove_nas: bool) -> Robj {
         ().into()
     }
+
+    /// Return the maximum of the elements in this vector.
+    /// If remove_nas is true, skip and NA values.
     fn max(&self, _remove_nas: bool) -> Robj {
         ().into()
     }
 }
 
 trait AltRealImpl: AltvecImpl {
+    /// Get a single element from this vector.
     fn elt(&self, _index: usize) -> f64 {
         0.0
     }
+
+    /// Get a multiple elements from this vector.
     fn get_region(&self, _index: usize, _data: &mut [i32]) -> usize {
         0
     }
+
+    /// Return true if this vector is sorted.
     fn is_sorted(&self) -> bool {
         false
     }
+
+    /// Return true if this vector does not contain NAs.
     fn no_na(&self) -> bool {
         false
     }
+
+    /// Return the sum of the elements in this vector.
+    /// If remove_nas is true, skip and NA values.
     fn sum(&self, _remove_nas: bool) -> Robj {
         ().into()
     }
+
+    /// Return the minimum of the elements in this vector.
+    /// If remove_nas is true, skip and NA values.
     fn min(&self, _remove_nas: bool) -> Robj {
         ().into()
     }
+
+    /// Return the maximum of the elements in this vector.
+    /// If remove_nas is true, skip and NA values.
     fn max(&self, _remove_nas: bool) -> Robj {
         ().into()
     }
 }
 
 trait AltLogicalImpl: AltvecImpl {
+    /// Get a single element from this vector.
     fn elt(&self, _index: usize) -> Bool {
         NA_LOGICAL
     }
+
+    /// Get a multiple elements from this vector.
     fn get_region(&self, _index: usize, _data: &mut [i32]) -> usize {
         0
     }
+
+    /// Return true if this vector is sorted.
     fn is_sorted(&self) -> bool {
         false
     }
+
+    /// Return true if this vector does not contain NAs.
     fn no_na(&self) -> bool {
         false
     }
+
+    /// Return the sum of the elements in this vector.
+    /// If remove_nas is true, skip and NA values.
     fn sum(&self, _remove_nas: bool) -> Robj {
         ().into()
     }
+
 }
 
 trait AltRawImpl: AltvecImpl {
+    /// Get a single element from this vector.
     fn elt(&self, _index: usize) -> u8 {
         0
     }
 
+    /// Get a multiple elements from this vector.
     fn get_region(&self, _index: usize, _data: &mut [u8]) -> usize {
         0
     }
 }
 
 trait AltComplexImpl: AltvecImpl {
-    fn altcomplex_elt(&self, _index: usize) -> Complex {
+    /// Get a single element from this vector.
+    fn elt(&self, _index: usize) -> Complex {
         Complex(0.0, 0.0)
     }
 
-    fn altcomplex_get_region(&self, _index: usize, _data: &mut [Complex]) -> usize {
+    /// Get a multiple elements from this vector.
+    fn get_region(&self, _index: usize, _data: &mut [Complex]) -> usize {
         0
     }
 }
 
 trait AltStringImpl: AltvecImpl {
+    /// Get a single element from this vector.
     fn elt(&self, _index: usize) -> Robj {
         ().into()
     }
-    fn set_elt(&self, _index: usize, _value: Robj) {}
+
+    /// Set a single element in this vector.
+    fn set_elt(&mut self, _index: usize, _value: Robj) {
+
+    }
+
+    /// Return true if this vector is sorted.
     fn is_sorted(&self) -> bool {
         false
     }
+
+    /// Return true if this vector does not contain NAs.
     fn no_na(&self) -> bool {
         false
     }
