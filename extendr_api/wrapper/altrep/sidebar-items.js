aa7157a52358124b07841ac71ec9fbc42e6ef5c0
andy-thomason <andy-thomason@users.noreply.github.com>
2021-08-15 23:43:02 +0000

    deploy: 1d85fc247c920ff408fa57fedae1c87f9cd92f5a

---
+++ extendr_api/wrapper/altrep/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Altrep",""]],"trait":[["AltComplexImpl",""],["AltIntegerImpl",""],["AltLogicalImpl",""],["AltRawImpl",""],["AltRealImpl",""],["AltStringImpl",""],["AltrepImpl","Rust trait for implementing ALTREP. Implement one or more of these methods to generate an Altrep class. This is likely to be unstable for a while."]]});
