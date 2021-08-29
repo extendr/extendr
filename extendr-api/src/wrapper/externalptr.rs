4f61b278d3c416ef13ff4631acf877501d30f622
andy-thomason <andy@andythomason.com>
2021-08-29 23:23:30 +0100

    More comments for Claus.

--- extendr-api/src/wrapper/externalptr.rs
+++ extendr-api/src/wrapper/externalptr.rs
@@ -16,31 +16,50 @@ use std::any::Any;
 ///
 #[derive(Debug, PartialEq, Clone)]
 pub struct ExternalPtr<T> {
+    /// This is the contained Robj.
     pub(crate) robj: Robj,
+
+    /// This is a zero-length object that holds the type of the object.
     marker: std::marker::PhantomData<T>,
 }
 
 impl<T> Deref for ExternalPtr<T> {
     type Target = T;
+
+    /// This allows us to treat the Robj as if it is the type T.
     fn deref(&self) -> &Self::Target {
         unsafe { &*self.robj.external_ptr_addr::<T>() }
     }
 }
 
 impl<T: Any> ExternalPtr<T> {
+    /// Construct an external pointer object from any type T.
     pub fn from_val(val: T) -> Self {
         unsafe {
+            // This gets the type name of T as a string. eg. "i32".
             let type_name = std::any::type_name::<T>();
+
+            // This allocates some memory for our object and moves the object into it.
             let boxed = Box::new(val);
+
+            // This constructs an external pointer to our boxed data.
+            // into_raw() converts the box to a malloced pointer.
             let robj = Robj::make_external_ptr(Box::into_raw(boxed), r!(type_name), r!(()));
 
             extern "C" fn finalizer<T>(x: SEXP) {
                 unsafe {
                     let ptr = R_ExternalPtrAddr(x) as *mut T;
+
+                    // Convert the pointer to a box and drop it implictly.
+                    // This frees up the memory we have used and calls the "T::drop" method if there is one.
                     Box::from_raw(ptr);
                 }
             }
+
+            // Tell R about our finalizer
             robj.register_c_finalizer(Some(finalizer::<T>));
+
+            // Return an object in a wrapper.
             Self {
                 robj,
                 marker: std::marker::PhantomData,
@@ -48,10 +67,12 @@ impl<T: Any> ExternalPtr<T> {
         }
     }
 
+    /// Get the "tag" of an external pointer. This is the type name in the common case.
     pub fn external_ptr_tag(&self) -> Robj {
         unsafe { new_owned(R_ExternalPtrTag(self.robj.get())) }
     }
 
+    /// Get the "protected" field of an external pointer. This is NULL in the common case.
     pub fn external_ptr_protected(&self) -> Robj {
         unsafe { new_owned(R_ExternalPtrProtected(self.robj.get())) }
     }
@@ -70,6 +91,7 @@ impl<T: Any> TryFrom<Robj> for ExternalPtr<T> {
             marker: std::marker::PhantomData,
         };
 
+        // Check the type name.
         let type_name = std::any::type_name::<T>();
         if res.external_ptr_tag().as_str() != Some(type_name) {
             return Err(Error::ExpectedExternalPtrType(res.robj, type_name.into()));
