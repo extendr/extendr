c3df0edc4b076e82cf9f3a94470c452e82e90ae8
Ilia-Kosenkov <Ilia-Kosenkov@users.noreply.github.com>
2021-09-03 05:05:45 +0000

    deploy: 6935c7dee0edc7e02c6355379376d8001167cd13

---
+++ implementors/extendr_api/prelude/trait.SliceArg.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["extendr_api"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
