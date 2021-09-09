b5a099cae1a576ea5baa50838ea9ff4dfd4795e8
andy-thomason <andy@andythomason.com>
2021-09-09 17:32:31 +0100

    fmt

--- extendr-api/src/prelude.rs
+++ extendr-api/src/prelude.rs
@@ -51,8 +51,8 @@ pub use super::thread_safety::{
 };
 
 pub use super::wrapper::{
-    EnvIter, Environment, Expression, ExternalPtr, FromList, Function, Integers, Language, List, ListIter,
-    Nullable, Pairlist, Primitive, Promise, Raw, Rstr, Symbol,
+    EnvIter, Environment, Expression, ExternalPtr, FromList, Function, Integers, Language, List,
+    ListIter, Nullable, Pairlist, Primitive, Promise, Raw, Rstr, Symbol,
 };
 
 #[cfg(feature = "ndarray")]
