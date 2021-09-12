1e6e60849e33190bc72f1cee38c7a10e3061c340
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-09-12 20:11:59 +0300

    Testing out of range

--- tests/extendrtests/tests/testthat/test-vector-wrappers.R
+++ tests/extendrtests/tests/testthat/test-vector-wrappers.R
@@ -2,4 +2,5 @@ test_that("Can access vector elements", {
     x <- c(42.0, NA_real_)
     expect_equal(get_doubles_element(x, 0), 42.0)
     expect_equal(get_doubles_element(x, 1), NA_real_)
+    expect_error(get_doubles_element(x, 2))
 })
