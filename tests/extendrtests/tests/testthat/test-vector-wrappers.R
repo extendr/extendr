64dff42010323bc82d9a9d934c0133b5dec0abde
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-09-15 17:22:29 +0300

    Updated tests

--- tests/extendrtests/tests/testthat/test-vector-wrappers.R
+++ tests/extendrtests/tests/testthat/test-vector-wrappers.R
@@ -1,28 +1,30 @@
 test_that("Access elements of double vector", {
     x <- c(42.0, NA_real_, 100.0)
     expect_equal(get_doubles_element(x, 0), 42.0)
-    expect_equal(get_doubles_element(x, 1), NA_real_)
-    expect_equal(get_doubles_element(x, 3), NA_real_)
+    expect_equal(get_doubles_element(x, 2), 100.0)
+    # Retrieving NA
+    expect_true(is.na(get_doubles_element(x, 1)))
+    # OOB returns NA
+    expect_true(is.na(get_doubles_element(x, 3)))
 })
 
 test_that("Access elements of integer vector", {
     x <- c(42L, NA_integer_, 100L)
     expect_equal(get_integers_element(x, 0), 42L)
-    expect_equal(get_integers_element(x, 1), NA_integer_)
-    expect_equal(get_integers_element(x, 3), NA_integer_)
+    expect_equal(get_integers_element(x, 2), 100L)
+    # Retrieving NA
+    expect_true(is.na(get_integers_element(x, 1)))
+    # OOB returns NA
+    expect_true(is.na(get_integers_element(x, 3)))
 })
 
 test_that("Construct double vector from squares of given values", {
-    x <- c(1.0 * (1:100), rep(NA_real_, 5), rep(NaN, 5))
-    x <- sample(x, length(x))
-
+    x <- c(1.0 * (1:100))
     expect_equal(doubles_square(x), x * x)
 })
 
 test_that("Construct integer vector from squares of given values", {
-    x <- c(1:100, rep(NA_integer_, 5))
-    x <- sample(x, length(x))
-
+    x <- c(1:100)
     expect_equal(integers_square(x), x * x)
 })
 
