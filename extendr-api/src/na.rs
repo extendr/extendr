0040698a329e29ec0ab4c206bcf28ab4cb9d5aae
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-09-21 23:00:04 +0300

    Fixing basic NA implementation

--- extendr-api/src/na.rs
+++ extendr-api/src/na.rs
@@ -1,4 +1,4 @@
-use libR_sys::R_IsNA;
+use libR_sys::{R_IsNA, R_NaReal};
 use crate::{Bool, na_str};
 /// Return true if this primitive is NA.
 pub trait IsNA {
@@ -10,17 +10,29 @@ impl IsNA for f64 {
     fn is_na(&self) -> bool {
         unsafe { R_IsNA(*self) != 0 }
     }
+
+    fn na() -> f64 {
+        unsafe{R_NaReal}
+    }
 }
 
 impl IsNA for i32 {
     fn is_na(&self) -> bool {
-        *self == std::i32::MIN
+        *self == i32::MIN
+    }
+
+    fn na() -> i32 {
+        i32::MIN
     }
 }
 
 impl IsNA for Bool {
     fn is_na(&self) -> bool {
-        self.0 == std::i32::MIN
+        self.0 == i32::MIN
+    }
+
+    fn na() -> Bool {
+        Bool::from(i32::MIN)
     }
 }
 
@@ -28,5 +40,9 @@ impl IsNA for &str {
     /// Check for NA in a string by address.
     fn is_na(&self) -> bool {
         self.as_ptr() == na_str().as_ptr()
+    }
+
+    fn na() -> Self {
+        na_str()
     }
 }
