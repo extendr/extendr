867a93d3efc8b2d1f7907dbe719895c488111718
andy-thomason <andy-thomason@users.noreply.github.com>
2021-10-20 09:25:29 +0000

    deploy: 2ab81bbce8c852d67a0f93bd2b7aeb0d3e058ba6

--- extendr_api/iter/sidebar-items.js
+++ extendr_api/iter/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["SliceIter","Generalised iterator of numbers and logical. See Int, Real and Logical."],["StrIter","Iterator over strings or string factors."]],"type":[["Int","Iterator over primitives in integer objects."],["Logical","Iterator over primitives in logical objects."],["NamedListIter","Iterator over name-value pairs in lists."],["Real","Iterator over primitives in real objects."]]});
+initSidebarItems({"struct":[["StrIter","Iterator over strings or string factors."]],"type":[["NamedListIter","Iterator over name-value pairs in lists."]]});
