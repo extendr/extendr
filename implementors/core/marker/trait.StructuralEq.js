44e5be57904f53c12369888aa60dd57623305baf
andy-thomason <andy-thomason@users.noreply.github.com>
2021-12-07 14:06:04 +0000

    deploy: 92bc46fd0c1086d6e40fc3d213748a8b86ca18f7

--- implementors/core/marker/trait.StructuralEq.js
+++ implementors/core/marker/trait.StructuralEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["extendr_api"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"extendr_api/logical/struct.Bool.html\" title=\"struct extendr_api::logical::Bool\">Bool</a>","synthetic":false,"types":["extendr_api::logical::Bool"]}];
+implementors["extendr_api"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"extendr_api/logical/struct.Bool.html\" title=\"struct extendr_api::logical::Bool\">Bool</a>","synthetic":false,"types":["extendr_api::logical::Bool"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
