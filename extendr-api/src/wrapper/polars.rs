cea5181a5f9621b67c57692f97c7055c827915a8
Michael Milton <michael.r.milton@gmail.com>
2022-01-13 02:09:47 +1100

    Minor formatting

--- extendr-api/src/wrapper/polars.rs
+++ extendr-api/src/wrapper/polars.rs
@@ -1,7 +1,7 @@
 use std::convert::TryFrom;
 
 use arrow2::ffi::*;
-use polars::prelude::{ArrowField, DataFrame, df, NamedFrom, Series};
+use polars::prelude::{df, ArrowField, DataFrame, NamedFrom, Series};
 
 use extendr_macros::{call, R};
 
@@ -15,8 +15,10 @@ impl TryFrom<DataFrame> for Robj {
     fn try_from(value: DataFrame) -> crate::Result<Self> {
         // Check if the `arrow` package is installed
         let pkg: &str = R!("system.file(package='arrow')")?.try_into()?;
-        if pkg.is_empty(){
-            return Err(Error::Other("The arrow package needs to be installed".into()))
+        if pkg.is_empty() {
+            return Err(Error::Other(
+                "The arrow package needs to be installed".into(),
+            ));
         }
 
         let (_nrow, ncol) = value.shape();
@@ -25,8 +27,8 @@ impl TryFrom<DataFrame> for Robj {
         // Make a list of name/arrow array pairs
         for series in value.get_columns() {
             // We push the arrays onto the heap and get a pointer to them
-            let array_ptr= Box::into_raw(Box::new(Ffi_ArrowArray::empty()));
-            let schema_ptr= Box::into_raw(Box::new(Ffi_ArrowSchema::empty()));
+            let array_ptr = Box::into_raw(Box::new(Ffi_ArrowArray::empty()));
+            let schema_ptr = Box::into_raw(Box::new(Ffi_ArrowSchema::empty()));
 
             // Rechunk the array so it's all contiguous
             let array_ref = series.rechunk().to_arrow(0);
@@ -35,7 +37,11 @@ impl TryFrom<DataFrame> for Robj {
                 export_array_to_c(array_ref, array_ptr);
                 export_field_to_c(field, schema_ptr);
             }
-            let imported_arr = call!("arrow::Array$import_from_c", array_ptr as usize, schema_ptr as usize)?;
+            let imported_arr = call!(
+                "arrow::Array$import_from_c",
+                array_ptr as usize,
+                schema_ptr as usize
+            )?;
             pairs.push((series.name().into(), imported_arr));
         }
 
@@ -48,16 +54,16 @@ impl TryFrom<DataFrame> for Robj {
 
 #[cfg(test)]
 mod test {
-    use crate::test;
     use super::*;
+    use crate::test;
 
-    fn assert_true_r(expr: Result<Robj>){
+    fn assert_true_r(expr: Result<Robj>) {
         assert!(bool::try_from(expr.unwrap()).unwrap())
     }
 
     #[test]
     fn test_df_to_robj() {
-        test!{
+        test! {
             let df = df!(
                 "a" => &[1, 2, 3, 4],
                 "b" => &[1., 2., 3., 4.],
@@ -67,6 +73,7 @@ mod test {
 
             // Check for the right subclass
             assert_true_r(R!("'RecordBatch' %in% class({{df_robj}})"));
+
             // These are all R6 methods that can only work if the data frame was properly
             // converted: https://arrow.apache.org/docs/r/reference/RecordBatch.html#r-methods
             assert_true_r(R!("{{df_robj}}$num_rows == 4"));
