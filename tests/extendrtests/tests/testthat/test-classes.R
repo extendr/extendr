4c1b7812d9009963bd3ea1ff101d4d38f46b11c4
shrektan <shrektan@126.com>
2022-01-14 23:38:29 +0800

    add tests for `[[`

--- tests/extendrtests/tests/testthat/test-classes.R
+++ tests/extendrtests/tests/testthat/test-classes.R
@@ -1,10 +1,13 @@
 test_that("Exported class works", {
   x <- MyClass$new()
   expect_equal(x$a(), 0L)
+  expect_equal(x[["a"]](), 0L)
   x$set_a(10L)
   expect_equal(x$a(), 10L)
+  expect_equal(x[["a"]](), 10L)
   expect_equal(x$me(), x)
-  
+  expect_equal(x[["me"]](), x)
+
   expect_visible(x$a())
   expect_invisible(x$set_a(5L))
 })
@@ -13,4 +16,5 @@ test_that("Unexported class works", {
   # unexported code works in testthat tests
   x <- MyClassUnexported$new()
   expect_equal(x$a(), 22L)
+  expect_equal(x[["a"]](), 22L)
 })
