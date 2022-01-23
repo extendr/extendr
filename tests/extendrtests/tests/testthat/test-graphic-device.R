235a24e9d9043eab81abd0ebf83e6d4685b67f99
Hiroaki Yutani <yutani.ini@gmail.com>
2022-01-24 00:13:50 +0900

    Another failing test

--- tests/extendrtests/tests/testthat/test-graphic-device.R
+++ tests/extendrtests/tests/testthat/test-graphic-device.R
@@ -4,5 +4,8 @@ test_that("`my_device()` works as expected", {
     # expect no error when various drawing operations are called
     expect_silent(plot(0))
 
+    # expect no error as well
+    expect_silent(grid::grid.newpage())
+
     expect_output(dev.off(), "good bye...", fixed = TRUE)
 })
