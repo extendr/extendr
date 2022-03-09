5ceda03384889c47306a5a4b1b2c525e08542e6a
andy-thomason <andy-thomason@users.noreply.github.com>
2022-03-09 11:41:16 +0000

    deploy: 09c8dbfe32cd48111d0d71c13d1fc085555a9576

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["extendr_api"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"extendr_api/robj/struct.Robj.html\" title=\"struct extendr_api::robj::Robj\">Robj</a>","synthetic":false,"types":["extendr_api::robj::Robj"]}];
+implementors["extendr_api"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"extendr_api/robj/struct.Robj.html\" title=\"struct extendr_api::robj::Robj\">Robj</a>","synthetic":false,"types":["extendr_api::robj::Robj"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
