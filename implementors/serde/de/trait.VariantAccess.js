b045e8754c7d6776b75b494082b94eb1f6c114bf
andy-thomason <andy-thomason@users.noreply.github.com>
2022-05-03 10:12:05 +0000

    deploy: 0dc5fc13249e54db5387b6ba1eb75f8efd12e61a

--- implementors/serde/de/trait.VariantAccess.js
+++ implementors/serde/de/trait.VariantAccess.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["extendr_api"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.136/serde/de/trait.VariantAccess.html\" title=\"trait serde::de::VariantAccess\">VariantAccess</a>&lt;'de&gt; for &amp;'de <a class=\"struct\" href=\"extendr_api/robj/struct.Robj.html\" title=\"struct extendr_api::robj::Robj\">Robj</a>","synthetic":false,"types":["extendr_api::robj::Robj"]}];
+implementors["extendr_api"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.137/serde/de/trait.VariantAccess.html\" title=\"trait serde::de::VariantAccess\">VariantAccess</a>&lt;'de&gt; for &amp;'de <a class=\"struct\" href=\"extendr_api/robj/struct.Robj.html\" title=\"struct extendr_api::robj::Robj\">Robj</a>","synthetic":false,"types":["extendr_api::robj::Robj"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/serde/ser/trait.Error.js
+++ implementors/serde/ser/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["extendr_api"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.136/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"extendr_api/error/enum.Error.html\" title=\"enum extendr_api::error::Error\">Error</a>","synthetic":false,"types":["extendr_api::error::Error"]}];
+implementors["extendr_api"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.137/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"extendr_api/error/enum.Error.html\" title=\"enum extendr_api::error::Error\">Error</a>","synthetic":false,"types":["extendr_api::error::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
