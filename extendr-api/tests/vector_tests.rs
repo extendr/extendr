bac2e16bb8733afd6468e09c3e54b6158b432d0b
andy-thomason <andy@atomicinrement.com>
2022-05-03 14:29:08 +0100

    Update comments

--- extendr-api/tests/vector_tests.rs
+++ extendr-api/tests/vector_tests.rs
@@ -269,7 +269,6 @@ fn test_doubles_iter() {
 #[test]
 fn test_doubles_from_values_short() {
     test! {
-        // Short (<64k) vectors are allocated.
         let vec = Doubles::from_values((0..3).map(|i| 2.0 - i as f64));
         assert_eq!(vec.is_altrep(), false);
         assert_eq!(r!(vec.clone()), r!([2.0, 1.0, 0.0]));
@@ -282,7 +281,6 @@ fn test_doubles_from_values_short() {
 #[test]
 fn test_doubles_from_values_altrep() {
     test! {
-        // Long (>=64k) vectors are lazy ALTREP objects.
         let vec = Doubles::from_values_altrep((0..1000000000).map(|x| x as f64));
         assert_eq!(vec.is_altrep(), true);
         assert_eq!(vec.elt(12345678), 12345678.0);
