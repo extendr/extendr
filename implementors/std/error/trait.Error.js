bcd0c116f374509a00d531360300f5ef150ed5bf
yutannihilation <yutannihilation@users.noreply.github.com>
2022-12-20 03:15:01 +0000

    deploy: d63a03974a417e4cd7c6daa1f126f6da4dcd472a

--- implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["extendr_api"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"extendr_api/error/enum.Error.html\" title=\"enum extendr_api::error::Error\">Error</a>","synthetic":false,"types":["extendr_api::error::Error"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
