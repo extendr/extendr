0ee0f890455d02d868d7550932c68da563f1ab4a
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-01-23 19:14:16 +0200

    Compare Rust dist with R dist

--- tests/extendrtests/tests/testthat/test-ndarray.R
+++ tests/extendrtests/tests/testthat/test-ndarray.R
@@ -1,8 +1,9 @@
 test_that("[ndarray] Euclidean distance", {
-    m <- matrix(1.0 * 1:6, nrow = 3, ncol = 2)
+    m <- matrix(1.0 * 1:100, nrow = 10, ncol = 10)
     dist <- euclidean_dist(m)
+    expected_dist <- dist(m, method = "euclidean") |> as.numeric()
 
-    expect_equal(dist, c(sqrt(2), 2 * sqrt(2), sqrt(2)))
+    expect_equal(dist, expected_dist)
 })
 
 test_that("[ndarray] Euclidean distance returns `NULL` when `NULL` is passed", {
