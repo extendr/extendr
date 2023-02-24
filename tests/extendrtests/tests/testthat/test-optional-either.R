027fa9a65e44f23a74a3aafe5a53eb09dd07fbb5
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-02-24 11:21:28 +0200

    Use {patrick} to test either

--- tests/extendrtests/tests/testthat/test-optional-either.R
+++ tests/extendrtests/tests/testthat/test-optional-either.R
@@ -1,26 +1,31 @@
-test_that("`type_aware_sum` works over ALTINT", {
-    input <- 1:10
+patrick::with_parameters_test_that(
+    "`type_aware_sum()` is type-stable and computes correct sums given",
+    {
+        result <- type_aware_sum(input)
 
-    result <- type_aware_sum(input)
+        vctrs::vec_assert(result, ptype = expected_ptype, size = 1L)
+        expect_equal(sum(input), result)
+    },
 
-    vctrs::vec_assert(result, ptype = integer(), size = 1L)
-    expect_equal(sum(input), result)
-})
+    patrick::cases(
+        ALTINT = list(input = 1:10, expected_ptype = integer()),
+        INTEGER = list(input = c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L), expected_ptype = integer()),
+        DOUBLE = list(input = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), expected_ptype = double()),
+        `Integers with NA` = list(input = c(1:5, NA_integer_), expected_ptype = integer())
+    )
+)
 
-test_that("`type_aware_sum` works over INTEGER", {
-    input <- c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L)
-
-    result <- type_aware_sum(input)
-
-    vctrs::vec_assert(result, ptype = integer(), size = 1L)
-    expect_equal(sum(input), result)
-})
-
-test_that("`type_aware_sum` works over DOUBLE", {
-    input <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
-    result <- type_aware_sum(input)
-
-    vctrs::vec_assert(result, ptype = double(), size = 1L)
-    expect_equal(sum(input), result)
-})
+patrick::with_parameters_test_that(
+    "`type_aware_sum()` does not accept types other than integers or doubles as input, given",
+    {
+        expect_error(type_aware_sum(input))
+    },
+    patrick::cases(
+        LOGICAL = list(input = c(TRUE, FALSE, NA)),
+        CHARACTER = list(input = letters),
+        COMPLEX = list(input = 1:10 + 1i * 11:20),
+        `NULL` = list(input = NULL),
+        `DATA.FRAME` = list(input = mtcars),
+        LIST = list(input = lapply(1:10, identity))
+    )
+)
