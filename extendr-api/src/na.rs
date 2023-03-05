d7ced40394750d47a1a57c41c537ba91d286325b
Mossa <cgmossa@gmail.com>
2023-03-05 23:08:31 +0100

    Document `CanBeNA` properly

--- extendr-api/src/na.rs
+++ extendr-api/src/na.rs
@@ -23,9 +23,11 @@ lazy_static! {
     };
 }
 
-/// Return true if this primitive is NA.
+/// Trait signifying if a given type may take the value `NA` in the R sense.
 pub trait CanBeNA {
+    /// Returns `true` if it is `NA`.
     fn is_na(&self) -> bool;
+    /// Returns the sentinel `NA` value for this type.
     fn na() -> Self;
 }
 
