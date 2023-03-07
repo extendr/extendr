b8a5227c33e76f43ed584e226c875a58ee4ca758
Mossa <cgmossa@gmail.com>
2023-03-07 20:38:20 +0100

    added From<NonZeroTypes> for Robj macro. I tried to do it without a macro and it wasn't succesful. Typically, you only get one blanket impl pr. type.

--- extendr-api/src/robj/into_robj.rs
+++ extendr-api/src/robj/into_robj.rs
@@ -377,6 +377,27 @@ impl ToVectorValue for Option<bool> {
     }
 }
 
+macro_rules! impl_into_robj_nonzero {
+    ($type: ty) => {
+        impl From<$type> for Robj {
+            fn from(value: $type) -> Self {
+                value.get().into()
+            }
+        }
+    };
+}
+
+impl_into_robj_nonzero!(std::num::NonZeroI8);
+impl_into_robj_nonzero!(std::num::NonZeroI16);
+impl_into_robj_nonzero!(std::num::NonZeroI32);
+impl_into_robj_nonzero!(std::num::NonZeroI64);
+// NOTE: `NonZeroIsize` is missing
+impl_into_robj_nonzero!(std::num::NonZeroU8);
+impl_into_robj_nonzero!(std::num::NonZeroU16);
+impl_into_robj_nonzero!(std::num::NonZeroU32);
+impl_into_robj_nonzero!(std::num::NonZeroU64);
+impl_into_robj_nonzero!(std::num::NonZeroUsize);
+
 // Not thread safe.
 fn fixed_size_collect<I>(iter: I, len: usize) -> Robj
 where
