b4700351ad6b65fcd34bc4e83f114d62975c3c12
CGMossa <cgmossa@gmail.com>
2023-03-10 14:09:00 +0100

    Revert "Removed `VECTOR_PTR` and also warnings on return type of throw r error function"
    This reverts commit b5a5350541cd2d219a8732a017732e052bc1cc1a.

--- extendr-api/src/error.rs
+++ extendr-api/src/error.rs
@@ -11,6 +11,7 @@ pub fn unwrap_or_throw<T>(r: std::result::Result<T, &'static str>) -> T {
     match r {
         Err(e) => {
             throw_r_error(e.to_string());
+            unreachable!("");
         }
         Ok(v) => v,
     }
@@ -21,6 +22,7 @@ pub fn unwrap_or_throw_error<T>(r: std::result::Result<T, Error>) -> T {
     match r {
         Err(e) => {
             throw_r_error(e.to_string());
+            unreachable!("");
         }
         Ok(v) => v,
     }
--- extendr-api/src/robj/mod.rs
+++ extendr-api/src/robj/mod.rs
@@ -788,6 +788,7 @@ make_typed_slice!(Rint, INTEGER, INTSXP);
 make_typed_slice!(f64, REAL, REALSXP);
 make_typed_slice!(Rfloat, REAL, REALSXP);
 make_typed_slice!(u8, RAW, RAWSXP);
+make_typed_slice!(Robj, VECTOR_PTR, VECSXP);
 make_typed_slice!(Rstr, STRING_PTR, STRSXP);
 make_typed_slice!(c64, COMPLEX, CPLXSXP);
 make_typed_slice!(Rcplx, COMPLEX, CPLXSXP);
