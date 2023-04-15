89f53136c4175cbe213209197d4dcf4e68a8e88a
Brian Carlsen <carlsen.bri@gmail.com>
2023-04-15 12:47:37 +0200

    Updated documentation for test function `raw_identifier_in_fn_args`.

--- tests/extendrtests/src/rust/src/raw_identifiers.rs
+++ tests/extendrtests/src/rust/src/raw_identifiers.rs
@@ -2,6 +2,7 @@ use extendr_api::prelude::*;
 
 /// Test raw identifiers (`r#`) in function arguments are parsed correctly.
 /// See (Issue #582)[https://github.com/extendr/extendr/issues/528] for details.
+/// @param type : i32 or `NULL`
 /// @export
 #[extendr(use_try_from = true)]
 fn raw_identifier_in_fn_args(#[default = "NULL"] r#type: Nullable<i32>) -> Nullable<i32> {
