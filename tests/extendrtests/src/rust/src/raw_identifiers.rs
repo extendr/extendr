56cb3f49a3fc3fc6b043ac25f5dc55ac8e8bf326
Brian Carlsen <carlsen.bri@gmail.com>
2023-04-27 09:46:07 +0200

    Fixed bugs in markdown hyperlinks.

--- tests/extendrtests/src/rust/src/raw_identifiers.rs
+++ tests/extendrtests/src/rust/src/raw_identifiers.rs
@@ -1,7 +1,7 @@
 use extendr_api::prelude::*;
 
 /// Test raw identifiers (`r#`) in function arguments are parsed correctly.
-/// See (Issue #582)[https://github.com/extendr/extendr/issues/528] for details.
+/// See [Issue #582](https://github.com/extendr/extendr/issues/528) for details.
 /// @param type : i32 or `NULL`
 /// @export
 #[extendr(use_try_from = true)]
@@ -10,7 +10,7 @@ fn raw_identifier_in_fn_args(#[default = "NULL"] r#type: Nullable<i32>) -> Nulla
 }
 
 /// Test raw identifiers (`r#`) as function names are parsed correctly.
-/// See (Issue #582)[https://github.com/extendr/extendr/issues/528] for details.
+/// See [Issue #582](https://github.com/extendr/extendr/issues/528) for details.
 /// @export
 #[extendr(use_try_from = true)]
 fn r#true() -> bool {
@@ -18,7 +18,7 @@ fn r#true() -> bool {
 }
 
 /// Combine raw identifiers (`r#`) as a function name and in arguments are parsed correctly.
-/// See (Issue #582)[https://github.com/extendr/extendr/issues/528] for details.
+/// See [Issue #582](https://github.com/extendr/extendr/issues/528) for details.
 /// @param type : i32 or `NULL`
 /// @export
 #[extendr(use_try_from = true)]
