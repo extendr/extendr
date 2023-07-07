d86cab9567b7de252cef5cc2cf9bad2372a38bf0
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-07-07 16:54:51 +0300

    Prototype conversion

--- extendr-api/src/wrapper/ellipsis.rs
+++ extendr-api/src/wrapper/ellipsis.rs
@@ -1,6 +1,31 @@
-use crate::Robj;
+use crate::prelude::*;
+use crate::{Error, Robj, Rtype, Types};
 
 #[derive(PartialEq, Clone)]
 pub struct Ellipsis {
     pub(crate) robj: Robj,
+}
+
+impl Ellipsis {}
+
+impl<'a> TryFrom<&'a Robj> for Ellipsis {
+    type Error = Error;
+
+    fn try_from(robj: &'a Robj) -> std::result::Result<Self, Self::Error> {
+        match robj.rtype() {
+            Rtype::Dot => Ok(Self { robj: robj.clone() }),
+            Rtype::Environment => try_from_env(robj),
+            _ => Err(Error::Other("Placeholder".into())),
+        }
+    }
+}
+
+fn try_from_env(env: &Robj) -> Result<Ellipsis> {
+    <Environment as TryFrom<&Robj>>::try_from(env)
+        .and_then(|e| {
+            e.iter()
+                .find(|(k, _)| *k == "...")
+                .ok_or(Error::Other("Ellipsis missing".into()))
+        })
+        .and_then(|(_, v)| <Ellipsis as TryFrom<&Robj>>::try_from(&v))
 }
