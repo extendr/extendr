9edcdf7d3e76ad8a2f818909d1a1022771fefdb3
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-07-07 17:28:37 +0300

    Prototype iteration

--- extendr-api/src/wrapper/ellipsis.rs
+++ extendr-api/src/wrapper/ellipsis.rs
@@ -1,12 +1,28 @@
 use crate::prelude::*;
-use crate::{Error, Robj, Rtype, Types};
+use crate::{Error, R_NilValue, Robj, Rtype, Types, SEXP};
 
 #[derive(PartialEq, Clone)]
 pub struct Ellipsis {
     pub(crate) robj: Robj,
 }
 
-impl Ellipsis {}
+impl Ellipsis {
+    pub fn iter(&self) -> EllipsisIter {
+        unsafe {
+            EllipsisIter {
+                robj: Some(&self.robj),
+                list_elem: self.robj.get(),
+            }
+        }
+    }
+
+    pub fn collect_values(&self) -> Result<Vec<Robj>> {
+        self.iter()
+            .filter_map(|x| <Promise as TryFrom<&Robj>>::try_from(&x).ok())
+            .map(|p| p.eval())
+            .collect()
+    }
+}
 
 impl<'a> TryFrom<&'a Robj> for Ellipsis {
     type Error = Error;
@@ -15,7 +31,7 @@ impl<'a> TryFrom<&'a Robj> for Ellipsis {
         match robj.rtype() {
             Rtype::Dot => Ok(Self { robj: robj.clone() }),
             Rtype::Environment => try_from_env(robj),
-            _ => Err(Error::Other("Placeholder".into())),
+            tp => Err(Error::Other(format!("Got {:?}", tp))),
         }
     }
 }
@@ -28,4 +44,43 @@ fn try_from_env(env: &Robj) -> Result<Ellipsis> {
                 .ok_or(Error::Other("Ellipsis missing".into()))
         })
         .and_then(|(_, v)| <Ellipsis as TryFrom<&Robj>>::try_from(&v))
+}
+
+#[derive(Clone)]
+pub struct EllipsisIter<'a> {
+    pub(crate) robj: Option<&'a Robj>,
+    pub(crate) list_elem: SEXP,
+}
+
+impl<'a> Default for EllipsisIter<'a> {
+    fn default() -> Self {
+        EllipsisIter::new()
+    }
+}
+
+impl<'a> EllipsisIter<'a> {
+    pub fn new() -> Self {
+        unsafe {
+            Self {
+                robj: None,
+                list_elem: R_NilValue,
+            }
+        }
+    }
+}
+
+impl<'a> Iterator for EllipsisIter<'a> {
+    type Item = Robj;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            if self.robj.is_none() || self.list_elem == R_NilValue {
+                return None;
+            }
+
+            let elem = libR_sys::CAR(self.list_elem);
+            self.list_elem = libR_sys::CDR(self.list_elem);
+            Some(Robj::from_sexp(elem))
+        }
+    }
 }
