7a12daf6a79edfe96899e6bd6c3be488f6f2c7ab
yutannihilation <yutannihilation@users.noreply.github.com>
2023-07-17 21:20:51 +0000

    deploy: d4bc4285be4401f0a21fa6d74b5fda3a2c805f83

--- src-files.js
+++ src-files.js
@@ -1,6 +1,6 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "extendr_api":["",[["graphics",[],["color.rs","device_descriptor.rs","device_driver.rs","mod.rs"]],["io",[],["load.rs","mod.rs","save.rs"]],["optional",[],["either.rs","mod.rs","ndarray.rs"]],["robj",[],["debug.rs","from_robj.rs","into_robj.rs","mod.rs","operators.rs","rinternals.rs","try_from_robj.rs"]],["scalar",[],["macros.rs","mod.rs","rbool.rs","rcplx_full.rs","rfloat.rs","rint.rs"]],["wrapper",[],["altrep.rs","complexes.rs","dataframe.rs","doubles.rs","environment.rs","expr.rs","externalptr.rs","function.rs","integers.rs","lang.rs","list.rs","logicals.rs","macros.rs","matrix.rs","mod.rs","nullable.rs","pairlist.rs","primitive.rs","promise.rs","raw.rs","rstr.rs","s4.rs","strings.rs","symbol.rs"]]],["deserializer.rs","error.rs","functions.rs","iter.rs","lang_macros.rs","lib.rs","metadata.rs","na.rs","ownership.rs","prelude.rs","rmacros.rs","serializer.rs","thread_safety.rs"]],\
 "extendr_engine":["",[],["lib.rs"]],\
 "extendr_macros":["",[],["R.rs","call.rs","dataframe.rs","extendr_function.rs","extendr_impl.rs","extendr_module.rs","lib.rs","list.rs","list_struct.rs","pairlist.rs","pairs.rs","wrappers.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
