be19004ac58956707236195b1f350cb2373313af
mossa <cgmossa@gmail.com>
2023-08-31 22:35:15 +0200

    `xtask` generates docs

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use xshell::{cmd, Shell};
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let shell = Shell::new()?;
+    let generate_docs = cmd!(
+        shell,
+        "cargo doc --workspace --no-deps --document-private-items --features full-functionality"
+    );
+
+    generate_docs.run()?;
+
+    Ok(())
 }
