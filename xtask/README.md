62a92e61fe6cd8b9ac8ead60a5e71ab579117685
mossa <cgmossa@gmail.com>
2023-08-31 23:15:15 +0200

    Added a bunch of options..  these need to be broken down..  and tested

--- xtask/README.md
+++ xtask/README.md
@@ -6,10 +6,13 @@ cargo xtask CMD
 
 Options for `CMD`:
 
+- [ ] `check_fmt`
+- [ ] `test_with_r`: Runs `R CMD check` and `testthat` tests in `tests/extendrtests`.
 - [ ] `doc`: Generates documentation as seen on [/extendr.github.io](https://extendr.github.io/extendr/extendr_api/)
-- 
+- [ ] `headers`: Copy R's C-headers to working directory
 
-## [NA] Addd `libgcc_eh` for Windows
+
+## [NA] Windows: Add `libgcc_eh` and `libgcc_s.a`
 
 Hang up is that once a linker is set, and the linker on Windows needing
 the presence of certain files to work, then the `xtask` doesn't compile,
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,14 +1,39 @@
 use xshell::{cmd, Shell};
 
-
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let shell = Shell::new()?;
+    // xtask
+    // shell.change_dir(std::env::var("CARGO_MANIFEST_DIR")?);
+    // dbg!(&shell.current_dir());
     let generate_docs = cmd!(
         shell,
         "cargo doc --workspace --no-deps --document-private-items --features full-functionality"
-    );
+    )
+    .run()?;
+    {
+        let _ = shell.push_dir(shell.current_dir().join("tests").join("extendrtests"));
+        let extendrtests = cmd!(shell, "R -e \"devtools::test()\" ").run()?;
+    }
 
-    generate_docs.run()?;
+    //TODO: Add option for `--no-build-vignettes`
+
+    let rcmdcheck_extendrtests =
+        cmd!(shell, "R CMD check --no-manual --as-cran --force-multiarch tests/extendrtests").run()?;
+
+    let fmt_check = cmd!(shell, "cargo fmt -- --check").run()?;
+
+    let msrv = cmd!(shell, "cargo-msrv --path extendr-api/ verify");
+    match msrv.run() {
+        Ok(_) => {}
+        Err(error) => {
+            //FIXME: Displays badly
+            return Err(format!(
+                "{}\n\nInstall `cargo-msrv` by `cargo install cargo-msrv`",
+                error.to_string()
+            )
+            .into());
+        }
+    }
 
     Ok(())
 }
