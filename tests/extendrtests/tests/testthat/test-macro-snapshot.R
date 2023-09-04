66d85ca764dd7171da2fbcf7b6b9c0c4a87c9310
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-09-04 11:38:18 +0300

    Skip test if no cargo expand

--- tests/extendrtests/tests/testthat/test-macro-snapshot.R
+++ tests/extendrtests/tests/testthat/test-macro-snapshot.R
@@ -1,8 +1,9 @@
 test_that("Macro expansion of lib.rs", {
-    expansion <- processx::run(
-        "cargo",
-        args = c("expand", "--manifest-path", "../../src/rust/Cargo.toml"),
-        error_on_status = FALSE
-    )
-    expect_snapshot_output(cat(expansion$stdout))
+  skip_if_no_cargo_expand()
+  expansion <- processx::run(
+    "cargo",
+    args = c("expand", "--manifest-path", "../../src/rust/Cargo.toml"),
+    error_on_status = FALSE
+  )
+  expect_snapshot_output(cat(expansion$stdout))
 })
