a80ff546bca3f268a2342487c77205d207411bfc
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-09-05 19:18:16 +0300

    Do not skip test if no cargo expand is present

--- tests/extendrtests/tests/testthat/test-macro-snapshot.R
+++ tests/extendrtests/tests/testthat/test-macro-snapshot.R
@@ -1,5 +1,5 @@
 test_that("Macro expansion of lib.rs", {
-  skip_if_no_cargo_expand()
+  # skip_if_no_cargo_expand()
   expansion <- processx::run(
     "cargo",
     args = c("expand", "--manifest-path", "../../src/rust/Cargo.toml"),
