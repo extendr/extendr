8ca13472c5cfe29477a6aaf8ded71ea9fd5db697
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-09-05 19:46:25 +0300

    Changes to restart CI

--- tests/extendrtests/tests/testthat/test-macro-snapshot.R
+++ tests/extendrtests/tests/testthat/test-macro-snapshot.R
@@ -1,5 +1,6 @@
 test_that("Macro expansion of lib.rs", {
   # skip_if_no_cargo_expand()
+
   expansion <- processx::run(
     "cargo",
     args = c("expand", "--manifest-path", "../../src/rust/Cargo.toml"),
