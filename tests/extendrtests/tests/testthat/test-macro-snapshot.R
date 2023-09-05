4fdaf24ce20bdd0c2df2670bb92bb55a78fa6bc1
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-09-05 20:05:32 +0300

    Run snapshot test when --as-cran

--- tests/extendrtests/tests/testthat/test-macro-snapshot.R
+++ tests/extendrtests/tests/testthat/test-macro-snapshot.R
@@ -6,5 +6,5 @@ test_that("Macro expansion of lib.rs", {
     args = c("expand", "--manifest-path", "../../src/rust/Cargo.toml"),
     error_on_status = FALSE
   )
-  expect_snapshot_output(cat(expansion$stdout))
+  expect_snapshot_output(cat(expansion$stdout), cran = TRUE)
 })
