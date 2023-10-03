d44aaa450d5b1686f62b028dc5c71f654c277c29
Hiroaki Yutani <yutani.ini@gmail.com>
2023-10-04 07:02:27 +0900

    Update syn crate to version 2 (#587)

--- extendr-macros/src/wrappers.rs
+++ extendr-macros/src/wrappers.rs
@@ -1,6 +1,6 @@
 use proc_macro2::Ident;
 use quote::{format_ident, quote};
-use syn::{parse_quote, punctuated::Punctuated, Expr, FnArg, ItemFn, Token, Type};
+use syn::{parse_quote, punctuated::Punctuated, Expr, ExprLit, FnArg, ItemFn, Token, Type};
 
 pub const META_PREFIX: &str = "meta__";
 pub const WRAP_PREFIX: &str = "wrap__";
@@ -193,18 +193,20 @@ pub fn make_function_wrappers(
 pub fn get_doc_string(attrs: &[syn::Attribute]) -> String {
     let mut res = String::new();
     for attr in attrs {
-        if let Some(id) = attr.path.get_ident() {
-            if *id != "doc" {
-                continue;
-            }
+        if !attr.path().is_ident("doc") {
+            continue;
+        }
 
-            if let Ok(syn::Meta::NameValue(nv)) = attr.parse_meta() {
-                if let syn::Lit::Str(litstr) = nv.lit {
-                    if !res.is_empty() {
-                        res.push('\n');
-                    }
-                    res.push_str(&litstr.value());
+        if let syn::Meta::NameValue(ref nv) = attr.meta {
+            if let Expr::Lit(ExprLit {
+                lit: syn::Lit::Str(ref litstr),
+                ..
+            }) = nv.value
+            {
+                if !res.is_empty() {
+                    res.push('\n');
                 }
+                res.push_str(&litstr.value());
             }
         }
     }
@@ -376,15 +378,20 @@ fn translate_actual(opts: &ExtendrOptions, input: &FnArg) -> Option<Expr> {
 fn get_named_lit(attrs: &mut Vec<syn::Attribute>, name: &str) -> Option<String> {
     let mut new_attrs = Vec::new();
     let mut res = None;
-    'f: for a in attrs.drain(0..) {
-        if let Ok(syn::Meta::NameValue(nv)) = a.parse_meta() {
+    for a in attrs.drain(0..) {
+        if let syn::Meta::NameValue(ref nv) = a.meta {
             if nv.path.is_ident(name) {
-                if let syn::Lit::Str(litstr) = nv.lit {
+                if let Expr::Lit(ExprLit {
+                    lit: syn::Lit::Str(ref litstr),
+                    ..
+                }) = nv.value
+                {
                     res = Some(litstr.value());
-                    continue 'f;
+                    continue;
                 }
             }
         }
+
         new_attrs.push(a);
     }
     *attrs = new_attrs;
