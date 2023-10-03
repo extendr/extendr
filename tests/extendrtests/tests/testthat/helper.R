293b98f9b02fb24c041e9bc843ec5e3d75d7bfe4
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-10-03 09:44:47 +0300

    Skip if toolchain is nightly

--- tests/extendrtests/tests/testthat/helper.R
+++ tests/extendrtests/tests/testthat/helper.R
@@ -10,3 +10,25 @@ skip_if_no_cargo_expand <- function() {
   condition <- (result$status == 0) && (!nzchar(result$stderr))
   skip_if_not(condition, "cargo expand not available")
 }
+
+skip_if_on_nightly <- function() {
+  result <- tryCatch(
+    processx::run(
+      "rustup",
+      args = "default",
+      error_on_status = FALSE
+    ),
+    error = function(e) list(status = 1)
+  )
+  if (result[["status"]] == 0) {
+    stdout <- result[["stdout"]]
+    condition <- stringi::stri_startswith_fixed(
+      stringi::stri_trim_left(stdout),
+      pattern = "nightly"
+    )
+  } else {
+    condition <- FALSE
+  }
+
+  skip_if(isTRUE(condition), "`nightly` toolchain")
+}
