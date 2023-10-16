bafa058e6064798aacbd4e39fb9a718243e0148f
yutannihilation <yutannihilation@users.noreply.github.com>
2023-10-16 23:36:11 +0000

    deploy: f39086eba1ec4e54469af39c77c1f806d3f2c6d4

--- implementors/serde/ser/trait.SerializeSeq.js
+++ implementors/serde/ser/trait.SerializeSeq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"extendr_api":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.188/serde/ser/trait.SerializeSeq.html\" title=\"trait serde::ser::SerializeSeq\">SerializeSeq</a> for <a class=\"struct\" href=\"extendr_api/serializer/struct.SerializeSeq.html\" title=\"struct extendr_api::serializer::SerializeSeq\">SerializeSeq</a>&lt;'a&gt;"]]
+"extendr_api":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.189/serde/ser/trait.SerializeSeq.html\" title=\"trait serde::ser::SerializeSeq\">SerializeSeq</a> for <a class=\"struct\" href=\"extendr_api/serializer/struct.SerializeSeq.html\" title=\"struct extendr_api::serializer::SerializeSeq\">SerializeSeq</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
