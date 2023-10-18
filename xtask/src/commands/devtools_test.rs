a3a272adb7622b223065fa51907072558d7537bb
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-10-19 01:40:29 +0300

    replace toml entry

--- xtask/src/commands/devtools_test.rs
+++ xtask/src/commands/devtools_test.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::path::Path;
 
-use toml_edit::Document;
+use toml_edit::{Document, InlineTable, Value};
 use xshell::Shell;
 
 pub(crate) fn run(shell: &Shell) -> Result<(), Box<dyn Error>> {
@@ -11,15 +11,40 @@ pub(crate) fn run(shell: &Shell) -> Result<(), Box<dyn Error>> {
     // }
 
     {
-        let _ = shell.push_dir("tests/extendrtests/src/rust");
+        let _rust_folder = shell.push_dir("tests/extendrtests/src/rust");
 
-        let (cargo_toml, is_crlf) = read_file_with_line_ending(&shell, "Cargo.toml")?;
+        let (original_cargo_toml, is_crlf) = read_file_with_line_ending(&shell, "Cargo.toml")?;
 
-        let cargo_toml: Document = cargo_toml.parse()?;
+        let original_cargo_toml: Document = original_cargo_toml.parse()?;
+
+        let mut cargo_toml = original_cargo_toml.clone();
+
+        let extendr_api_entry = get_extendr_api_entry(&mut cargo_toml)
+            .ok_or("`extendr-api` not found in Cargo.toml")?;
+
+        let mut replacement = InlineTable::new();
+        let item = Value::from(
+            shell
+                .current_dir()
+                .into_os_string()
+                .to_string_lossy()
+                .to_string()
+                .replace("\\", "/"),
+        );
+        replacement.entry("path").or_insert(item);
+        *extendr_api_entry = Value::InlineTable(replacement);
 
         write_file_preserve_line_ending(&shell, "Cargo.toml", &cargo_toml, is_crlf)?;
     }
     unreachable!("devtools::test()")
+}
+
+fn get_extendr_api_entry(document: &mut Document) -> Option<&mut Value> {
+    document
+        .get_mut("patch")?
+        .get_mut("crates-io")?
+        .get_mut("extendr-api")?
+        .as_value_mut()
 }
 
 fn read_file_with_line_ending<P: AsRef<Path>>(
