7ad657ff124b89772e26e88c8d375627baf12cb6
mossa <cgmossa@gmail.com>
2023-10-22 17:58:47 +0200

    Added `cargo xtask msrv`

--- xtask/src/commands/cargo_msrv.rs
+++ xtask/src/commands/cargo_msrv.rs
@@ -1,18 +1,9 @@
-use xshell::{Error, Shell};
+use xshell::{cmd, Error, Shell};
 
-pub(crate) fn run(_shell: &Shell) -> Result<(), Error> {
-    // let msrv = cmd!(shell, "cargo-msrv --path extendr-api/ verify");
-    // match msrv.run() {
-    //     Ok(_) => {}
-    //     Err(error) => {
-    //         //FIXME: Displays badly
-    //         return Err(format!(
-    //             "{}\n\nInstall `cargo-msrv` by `cargo install cargo-msrv`",
-    //             error.to_string()
-    //         )
-    //         .into());
-    //     }
-    // }
-    //
-    unimplemented!("cargo-msrv --path extendr-api")
+pub(crate) fn run(shell: &Shell) -> Result<(), Error> {
+    let msrv = cmd!(shell, "cargo-msrv --path extendr-api verify").run();
+    if msrv.is_err() {
+        println!("Cannot perform `cargo-msrv` check\nInstall `cargo-msrv" by `cargo install cargo-msrv`);
+    }
+    msrv
 }
