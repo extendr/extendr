52b77b25208e62215ff62f8de9ea7890c2506fab
mossa <cgmossa@gmail.com>
2023-10-22 17:29:54 +0200

    Made the formatting check command work! [skip ci]

--- xtask/src/commands/cargo_fmt_check.rs
+++ xtask/src/commands/cargo_fmt_check.rs
@@ -1,5 +1,12 @@
 use xshell::{cmd, Error, Shell};
 
 pub(crate) fn run(shell: &Shell) -> Result<(), Error> {
-    cmd!(shell, "cargo fmt -- --check").run()
+    let check_result = cmd!(shell, "cargo fmt -- --check").run();
+    if check_result.is_err() {
+        println!("Please run `cargo fmt --all`");
+    } else {
+        println!("Success!");
+    }
+
+    check_result
 }
