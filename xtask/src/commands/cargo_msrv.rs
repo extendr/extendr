102affe4d41aee4c3e64e4d0367f3882b0b414ab
mossa <cgmossa@gmail.com>
2023-10-22 18:10:12 +0200

    Forgot quote [skip ci]

--- xtask/src/commands/cargo_msrv.rs
+++ xtask/src/commands/cargo_msrv.rs
@@ -3,7 +3,7 @@ use xshell::{cmd, Error, Shell};
 pub(crate) fn run(shell: &Shell) -> Result<(), Error> {
     let msrv = cmd!(shell, "cargo-msrv --path extendr-api verify").run();
     if msrv.is_err() {
-        println!("Cannot perform `cargo-msrv` check\nInstall `cargo-msrv" by `cargo install cargo-msrv`);
+        println!("Cannot perform `cargo-msrv` check\nInstall `cargo-msrv` by `cargo install cargo-msrv`");
     }
     msrv
 }
