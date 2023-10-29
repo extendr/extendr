e4a284c1feef8f8246fe006c987e068718a4b41f
cgmossa <cgmossa@gmail.com>
2023-10-29 08:01:46 +0100

    Revert "Include `result_list`, and `result_condition` in tests"
    This reverts commit 977aa839b0d1315bf72c4b86eedd2e7fa27f1914.

--- libgcc_mock.ps1
+++
@@ -1,6 +0,0 @@
-# create a directory in an arbitrary location (e.g. libgcc_mock)
-New-Item -Path libgcc_mock -Type Directory
-
-# create empty libgcc_eh.a and libgcc_s.a
-New-Item -Path libgcc_mock\libgcc_eh.a -Type File
-New-Item -Path libgcc_mock\libgcc_s.a -Type File
