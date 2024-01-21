d0951c721c6dd18991bd914f36a43d08d82decc0
cgmossa <cgmossa@gmail.com>
2024-01-21 15:43:29 +0100

    clippy: suggestions

--- xtask/src/commands/generate_docs.rs
+++ xtask/src/commands/generate_docs.rs
@@ -2,7 +2,7 @@ use xshell::{cmd, Error, Shell};
 
 /// Generates documentation like on the site extendr.github.io
 pub(crate) fn run(shell: &Shell) -> Result<(), Error> {
-    let _generate_docs = cmd!(
+    cmd!(
         shell,
         "cargo doc --workspace --no-deps --document-private-items --features full-functionality"
     )
