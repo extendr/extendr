4c1cb5c3b98733d217ee3f63a76a7ab0ead5be94
cgmossa <cgmossa@gmail.com>
2024-01-28 17:21:34 +0100

    fix: extendrtests points to libR-sys submodule updated `xtask` as well

--- xtask/src/extendrtests/with_absolute_path.rs
+++ xtask/src/extendrtests/with_absolute_path.rs
@@ -41,9 +41,19 @@ pub(crate) fn swap_extendr_api_path(shell: &Shell) -> Result<DocumentHandle, Box
 
     let mut replacement = InlineTable::new();
 
-    let item = Value::from(get_replacement_path(&current_path));
+    let item = Value::from(get_replacement_path_extendr_api(&current_path));
     replacement.entry("path").or_insert(item);
     *extendr_api_entry = Value::InlineTable(replacement);
+
+    #[allow(non_snake_case)]
+    let libR_sys_entry =
+        get_libR_sys_entry(&mut cargo_toml).ok_or("`libR-sys` not found in Cargo.toml")?;
+
+    let mut replacement = InlineTable::new();
+
+    let item = Value::from(get_replacement_path_libR_sys(&current_path));
+    replacement.entry("path").or_insert(item);
+    *libR_sys_entry = Value::InlineTable(replacement);
 
     shell.write_file(CARGO_TOML, cargo_toml.to_string())?;
     Ok(DocumentHandle {
@@ -52,10 +62,17 @@ pub(crate) fn swap_extendr_api_path(shell: &Shell) -> Result<DocumentHandle, Box
     })
 }
 
-fn get_replacement_path(path: &Path) -> String {
+fn get_replacement_path_extendr_api(path: &Path) -> String {
     let path = path.adjust_for_r();
 
     format!("{path}/extendr-api")
+}
+
+#[allow(non_snake_case)]
+fn get_replacement_path_libR_sys(path: &Path) -> String {
+    let path = path.adjust_for_r();
+
+    format!("{path}/libR-sys")
 }
 
 fn get_extendr_api_entry(document: &mut Document) -> Option<&mut Value> {
@@ -63,6 +80,15 @@ fn get_extendr_api_entry(document: &mut Document) -> Option<&mut Value> {
         .get_mut("patch")?
         .get_mut("crates-io")?
         .get_mut("extendr-api")?
+        .as_value_mut()
+}
+
+#[allow(non_snake_case)]
+fn get_libR_sys_entry(document: &mut Document) -> Option<&mut Value> {
+    document
+        .get_mut("patch")?
+        .get_mut("crates-io")?
+        .get_mut("libR-sys")?
         .as_value_mut()
 }
 
