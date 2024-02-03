2487a00f25c63c1623759e3eb40e7fa1036edab7
Ilia-Kosenkov <Ilia-Kosenkov@users.noreply.github.com>
2024-02-03 17:46:04 +0000

    deploy: a17dfacec8464028c2a53646043788332a17492a

--- trait.impl/serde/de/trait.MapAccess.js
+++ trait.impl/serde/de/trait.MapAccess.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"extendr_api":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.195/serde/de/trait.MapAccess.html\" title=\"trait serde::de::MapAccess\">MapAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"extendr_api/deserializer/struct.NamedListGetter.html\" title=\"struct extendr_api::deserializer::NamedListGetter\">NamedListGetter</a>&lt;'de&gt;"]]
+"extendr_api":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.196/serde/de/trait.MapAccess.html\" title=\"trait serde::de::MapAccess\">MapAccess</a>&lt;'de&gt; for <a class=\"struct\" href=\"extendr_api/deserializer/struct.NamedListGetter.html\" title=\"struct extendr_api::deserializer::NamedListGetter\">NamedListGetter</a>&lt;'de&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/serde/ser/trait.SerializeTupleStruct.js
+++ trait.impl/serde/ser/trait.SerializeTupleStruct.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"extendr_api":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.195/serde/ser/trait.SerializeTupleStruct.html\" title=\"trait serde::ser::SerializeTupleStruct\">SerializeTupleStruct</a> for <a class=\"struct\" href=\"extendr_api/serializer/struct.SerializeTupleStruct.html\" title=\"struct extendr_api::serializer::SerializeTupleStruct\">SerializeTupleStruct</a>&lt;'a&gt;"]]
+"extendr_api":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.196/serde/ser/trait.SerializeTupleStruct.html\" title=\"trait serde::ser::SerializeTupleStruct\">SerializeTupleStruct</a> for <a class=\"struct\" href=\"extendr_api/serializer/struct.SerializeTupleStruct.html\" title=\"struct extendr_api::serializer::SerializeTupleStruct\">SerializeTupleStruct</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
