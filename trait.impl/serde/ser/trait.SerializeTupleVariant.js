19cbf6bead48e43462d37053c1889b0a81523487
JosiahParry <JosiahParry@users.noreply.github.com>
2024-02-29 22:11:08 +0000

    deploy: 0d82b42574281e0091a11aca5ee00a3819461c18

--- trait.impl/serde/ser/trait.SerializeTupleVariant.js
+++ trait.impl/serde/ser/trait.SerializeTupleVariant.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"extendr_api":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.196/serde/ser/trait.SerializeTupleVariant.html\" title=\"trait serde::ser::SerializeTupleVariant\">SerializeTupleVariant</a> for <a class=\"struct\" href=\"extendr_api/serializer/struct.SerializeTupleVariant.html\" title=\"struct extendr_api::serializer::SerializeTupleVariant\">SerializeTupleVariant</a>&lt;'a&gt;"]]
+"extendr_api":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.197/serde/ser/trait.SerializeTupleVariant.html\" title=\"trait serde::ser::SerializeTupleVariant\">SerializeTupleVariant</a> for <a class=\"struct\" href=\"extendr_api/serializer/struct.SerializeTupleVariant.html\" title=\"struct extendr_api::serializer::SerializeTupleVariant\">SerializeTupleVariant</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
