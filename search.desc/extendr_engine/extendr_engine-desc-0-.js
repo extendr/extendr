6a8f4f645424191e392ec9ad9e1574180b9baafd
Ilia-Kosenkov <Ilia-Kosenkov@users.noreply.github.com>
2024-04-09 19:54:16 +0000

    deploy: 70720e2a4b6b7b3b61a786639f14364a65ec9945

---
+++ search.desc/extendr_engine/extendr_engine-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("extendr_engine", 0, "A singleton instance of the R interpreter.\nClose down the R interpreter. Note you won’t be able to …\nEnsures that an embedded R instance is present when …")
