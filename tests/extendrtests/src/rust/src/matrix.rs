961e8c86d830203bcfc734329c3597caa6079549
Mossa <cgmossa@gmail.com>
2024-06-16 16:09:15 +0200

    wp: took the tests from #800. I couldn't find a better way to port them over.

---
+++ tests/extendrtests/src/rust/src/matrix.rs
@@ -0,0 +1,31 @@
+use extendr_api::prelude::*;
+
+#[extendr]
+fn fetch_dimnames(x: RMatrix<f64>) -> Robj {
+    x.get_dimnames().into()
+}
+
+#[extendr]
+fn fetch_rownames(x: RMatrix<f64>) -> Robj {
+    x.get_rownames()
+}
+
+#[extendr]
+fn fetch_colnames(x: RMatrix<f64>) -> Robj {
+    x.get_colnames()
+}
+
+#[extendr]
+fn change_dimnames(mut x: RMatrix<f64>) -> Robj {
+    let rownames = Strings::from_values(["AA", "BB", "CC"]);
+    x.set_dimnames(list!(rownames, NULL));
+    x.to_owned()
+}
+
+extendr_module! {
+    mod matrix;
+    fn fetch_colnames;
+    fn fetch_rownames;
+    fn fetch_dimnames;
+    fn change_dimnames;
+}
---
+++ tests/extendrtests/tests/testthat/test-matrix.R
@@ -0,0 +1,26 @@
+dims <- list(c("a", "b", "c"), c("d", "e", "f"))
+
+m <- matrix(
+  as.double(1:9),
+  nrow = 3,
+  dimnames = dims
+)
+
+
+test_that("Dimnames can be fetched", {
+  expect_equal(fetch_dimnames(m), dims)
+})
+
+test_that("Rownames can be fetched", {
+  expect_equal(fetch_rownames(m), dims[[1]])
+})
+
+test_that("Rownames can be fetched", {
+  expect_equal(fetch_colnames(m), dims[[2]])
+})
+
+test_that("Dimnames can be changed", {
+  m2 <- m
+  dimnames(m2) <- list(c("AA", "BB", "CC"), NULL)
+  expect_equal(change_dimnames(m), m2)
+})
