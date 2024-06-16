cb8c1f3c82d108d700540e076217d2a3967b9e3f
CGMossa <cgmossa@gmail.com>
2024-06-16 19:03:19 +0200

    Update extendr-api/tests/into_iterator_tests.rs
    Co-authored-by: Josiah Parry <josiah.parry@gmail.com>

--- extendr-api/tests/into_iterator_tests.rs
+++ extendr-api/tests/into_iterator_tests.rs
@@ -5,6 +5,7 @@ use extendr_engine::with_r;
 fn iterating_unamed_list() {
     with_r(|| {
         let unamed_list = List::from_values([1, 2, 3, 42, 100]);
+        assert_eq!(unamed_list.names(), None);
         assert_eq!(unamed_list.iter().len(), 5);
         let unamed_list = List::from_values::<[i32; 0]>([]);
         assert_eq!(unamed_list.iter().len(), 0);
