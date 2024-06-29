d922c894ac2d655767b998f549ae5c5284fc0e58
JosiahParry <JosiahParry@users.noreply.github.com>
2024-06-29 21:27:36 +0000

    deploy: d39d1d09ea024a31c3331a754baeafe3c7ae5b0e

--- extendr_api/prelude/stats/sidebar-items.js
+++ extendr_api/prelude/stats/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Normal","NormalCol","NormalMat","NormalRow","StandardCol","StandardMat","StandardNormalCol","StandardNormalMat","StandardNormalRow","StandardRow","UnitaryMat"]};
+window.SIDEBAR_ITEMS = {"enum":["NanHandling"],"fn":["col_mean","col_varm","row_mean","row_varm"],"struct":["Normal","NormalCol","NormalMat","NormalRow","StandardCol","StandardMat","StandardNormalCol","StandardNormalMat","StandardNormalRow","StandardRow","UnitaryMat"]};
