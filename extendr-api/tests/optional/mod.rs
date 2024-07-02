596f464b1df569673e852010a9100c97755bb8dd
Mossa <cgmossa@gmail.com>
2024-07-02 13:34:35 +0200

    Doc: added documentation of this in the integration test module itself.

--- extendr-api/tests/optional/mod.rs
+++ extendr-api/tests/optional/mod.rs
@@ -1,3 +1,22 @@
+//! Tests for the optional (additive) features of `extendr-api`
+//!
+//!
+//! These are invoked via
+//!
+//! ```sh
+//! cargo test --test features --feature <features required>
+//! ```
+//!
+//! For this purpose, the feature `full-functionality` ought to represent all
+//! the additive features to `extendr-api`, i.e.
+//!
+//! ```sh
+//! cargo test --test features --feature full-functionality
+//! ```
+//!
+//! should run all the tests defined here.
+//!
+
 #[cfg(feature = "either")]
 mod either;
 
