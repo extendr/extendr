37cdf54d8fa0a6c77292fa2854cb0d0db394be42
CGMossa <CGMossa@users.noreply.github.com>
2024-07-07 17:06:03 +0000

    deploy: 0d0a525b59a1c07c16e0be3e16cabc513f471ced

--- search.desc/extendr_engine/extendr_engine-desc-0-.js
+++ search.desc/extendr_engine/extendr_engine-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("extendr_engine", 0, "A singleton instance of the R interpreter.\nClose down the R interpreter. Note you won’t be able to …\nEnsures that an embedded R instance is present when …")
+searchState.loadedDescShard("extendr_engine", 0, "Embeds a a single R process\nClose down the R interpreter. Note you won’t be able to …\nEnsures that an embedded R instance is present when …")
