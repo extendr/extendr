dd2731133c25afafb4f52bc22dcba3e01778322e
Josiah Parry <josiah.parry@gmail.com>
2024-09-03 12:28:49 -0700

    hide is_valid_string behind non-api flag (#842)

--- extendr-api/src/robj/rinternals.rs
+++ extendr-api/src/robj/rinternals.rs
@@ -354,11 +354,13 @@ pub trait Rinternals: Types + Conversions {
         unsafe { Rf_isUserBinop(self.get()).into() }
     }
 
+    #[cfg(feature = "non-api")]
     /// Return true if this is a valid string.
     fn is_valid_string(&self) -> bool {
         unsafe { Rf_isValidString(self.get()).into() }
     }
 
+    #[cfg(feature = "non-api")]
     /// Return true if this is a valid string.
     fn is_valid_string_f(&self) -> bool {
         unsafe { Rf_isValidStringF(self.get()).into() }
