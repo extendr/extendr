9b78700f3354529f9404f688ddd6dc03efd8c719
Josiah Parry <josiah.parry@gmail.com>
2024-12-16 13:54:01 -0800

    add tests for array types

--- tests/extendrtests/tests/testthat/test-tuples.R
+++ tests/extendrtests/tests/testthat/test-tuples.R
@@ -11,3 +11,62 @@ test_that("tuple conversions work", {
   # error works as anticipated
   expect_error(sum_triplet_ints(as.list(1:3)), "Expected length")
 })
+
+test_that("tuple array f64", {
+  x <- rnorm(4)
+  expect_identical(round_trip_array_f64(x), x)
+  expect_error(round_trip_array_f64(x[1:3]))
+  expect_error(round_trip_array_f64(rnorm(10)))
+})
+
+test_that("tuple array Rfloat", {
+  x <- rnorm(4)
+  x[2] <- NA
+  expect_identical(round_trip_array_rfloat(x), x)
+  expect_error(round_trip_array_rfloat(x[1:3]))
+  expect_error(round_trip_array_rfloat(rep(x, 3)))
+})
+
+
+test_that("tuple array Rint", {
+  x <- c(0L, NA, 2L, 1L)
+  expect_identical(round_trip_array_rint(x), x)
+  expect_error(round_trip_array_rint(x[1:3]))
+  expect_error(round_trip_array_rint(rep(x, 3)))
+})
+
+test_that("tuple array i32", {
+  x <- c(0L, NA, 2L, 1L)
+  expect_identical(round_trip_array_i32(x), x)
+  expect_error(round_trip_array_i32(x[1:3]))
+  expect_error(round_trip_array_i32(rep(x, 3)))
+})
+
+test_that("tuple array Rbool", {
+  x <- c(TRUE, FALSE, NA, TRUE)
+  expect_identical(round_trip_array_rbool(x), x)
+  expect_error(round_trip_array_rbool(x[1:3]))
+  expect_error(round_trip_array_rbool(rep(x, 3)))
+})
+
+# Create a vector of 4 complex numbers
+
+
+test_that("tuple array complex", {
+  x <- c(1 + 2i, NA, 5 - 6i, 7 - 8i)
+  expect_identical(round_trip_array_rcplx(x), x)
+  expect_error(round_trip_array_rcplx(x[1:3]))
+  expect_error(round_trip_array_rcplx(rep(x, 3)))
+})
+
+test_that("tuple array u8", {
+  x <- charToRaw("abc!")
+  expect_identical(round_trip_array_u8(x), x)
+  expect_error(round_trip_array_u8(x[1:3]))
+  expect_error(round_trip_array_u8(rep(x, 3)))
+})
+
+
+
+
+
