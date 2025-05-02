bf216dc4b6ccda9c0cf9d2025f469654338cbb5c
Mossa <cgmossa@gmail.com>
2025-05-02 22:39:12 +0200

    xtask: added `--features` argument to `xtask`

--- xtask/src/commands/cargo_msrv.rs
+++ xtask/src/commands/cargo_msrv.rs
@@ -1,7 +1,13 @@
 use xshell::{cmd, Error, Shell};
 
-pub(crate) fn run(shell: &Shell) -> Result<(), Error> {
-    let msrv = cmd!(shell, "cargo-msrv --path extendr-api verify").run();
+pub(crate) fn run(shell: &Shell, features: Option<Vec<String>>) -> Result<(), Error> {
+    let features_arg = features.map(|x| x.join(",")).unwrap_or_default();
+
+    let msrv = cmd!(
+        shell,
+        "cargo-msrv --path extendr-api verify -- cargo check --features={features_arg}"
+    )
+    .run();
     if msrv.is_err() {
         println!(
             "Cannot perform `cargo-msrv` check\nInstall `cargo-msrv` by `cargo install cargo-msrv`"
