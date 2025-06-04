7097896f17000100e1ee52bb5bf0e73de16caf12
Mossa <cgmossa@gmail.com>
2025-06-05 00:06:32 +0200

    updated tests/extendrtests to follow rextendr template
    (cherry picked from commit 507644d0db3ee98a74035b1e050987fb200ffe66)

--- tests/extendrtests/src/entrypoint.c
+++ tests/extendrtests/src/entrypoint.c
@@ -1,8 +1,8 @@
-// We need to forward routine registration from C to Rust 
+// We need to forward routine registration from C to Rust
 // to avoid the linker removing the static library.
 
 void R_init_extendrtests_extendr(void *dll);
 
 void R_init_extendrtests(void *dll) {
-  R_init_extendrtests_extendr(dll);
+    R_init_extendrtests_extendr(dll);
 }
