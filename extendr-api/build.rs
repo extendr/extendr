4a53ecbedc6f55b9024faa76899ddc6ac144cc93
Josiah Parry <josiah.parry@gmail.com>
2025-10-25 11:47:20 -0400

    fix: R graphics engine version  (#959)
    * update graphics version
    
    * add configs for graphic engine version
    
    * remove GE version 14 get GE version matches correct

--- extendr-api/build.rs
+++ extendr-api/build.rs
@@ -3,8 +3,9 @@ use std::env;
 fn main() {
     println!("cargo:rustc-check-cfg=cfg(use_objsxp)");
     println!("cargo:rustc-check-cfg=cfg(use_r_newenv)");
-    println!("cargo:rustc-check-cfg=cfg(use_r_ge_version_14)");
     println!("cargo:rustc-check-cfg=cfg(use_r_ge_version_15)");
+    println!("cargo:rustc-check-cfg=cfg(use_r_ge_version_16)");
+    println!("cargo:rustc-check-cfg=cfg(use_r_ge_version_17)");
     println!("cargo:rustc-check-cfg=cfg(use_r_altlist)");
 
     // The R version information is needed to handle the API differences
@@ -22,15 +23,20 @@ fn main() {
         println!("cargo:rustc-cfg=use_r_newenv");
     }
 
-    // pattern fill was introduced in R 4.1
-    if &*major >= "4" && &*minor >= "1" {
-        println!("cargo:rustc-cfg=use_r_ge_version_14");
-    }
-
     // a few new features will be introduced in R 4.2
     // c.f. https://developer.r-project.org/Blog/public/2021/12/14/updating-graphics-devices-for-r-4.2.0/index.html
     if &*major >= "4" && &*minor >= "2" {
         println!("cargo:rustc-cfg=use_r_ge_version_15");
+    }
+
+    // Graphics engine version 16 was introduced in R 4.3
+    if &*major >= "4" && &*minor >= "3" {
+        println!("cargo:rustc-cfg=use_r_ge_version_16");
+    }
+
+    // Graphics engine version 17 was introduced in R 4.6
+    if &*major >= "4" && &*minor >= "6" {
+        println!("cargo:rustc-cfg=use_r_ge_version_17");
     }
 
     if &*major >= "4" && &*minor >= "3" {
