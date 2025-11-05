beb0dd08b43c12e716277d5e4f9a8e923cde5c25
Mossa <cgmossa@gmail.com>
2025-11-05 18:53:59 +0100

    fix typo

--- xtask/src/commands/cargo_msrv.rs
+++ xtask/src/commands/cargo_msrv.rs
@@ -1,17 +1,24 @@
 use xshell::{cmd, Error, Shell};
 
 pub(crate) fn run(shell: &Shell, features: Option<Vec<String>>) -> Result<(), Error> {
-    let features_arg = features.map(|x| x.join(",")).unwrap_or_default();
+    let msrv = match features {
+        Some(feats) if !feats.is_empty() => {
+            let features = feats.join(",");
+            cmd!(
+                shell,
+                "cargo-msrv --path extendr-api verify -- cargo check --features {features}"
+            )
+            .run()
+        }
+        _ => cmd!(shell, "cargo-msrv --path extendr-api verify -- cargo check").run(),
+    };
 
-    let msrv = cmd!(
-        shell,
-        "cargo-msrv --path extendr-api verify -- cargo check --features={features_arg}"
-    )
-    .run();
-    if msrv.is_err() {
-        println!(
-            "Cannot perform `cargo-msrv` check\nInstall `cargo-msrv` by `cargo install cargo-msrv`"
+    if let Err(ref e) = msrv {
+        eprintln!(
+            "Cannot perform `cargo-msrv` check: {e}\n\
+             Install `cargo-msrv` with `cargo install cargo-msrv`"
         );
     }
+
     msrv
 }
