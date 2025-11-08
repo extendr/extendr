a0ef604af237593b0b5ad3d444818a4519db62bc
Mossa <cgmossa@gmail.com>
2025-11-08 19:40:25 +0100

    show leak on stack

---
+++ tests/extendrtests/src/rust/src/leak.rs
@@ -0,0 +1,35 @@
+use extendr_api::prelude::*;
+
+struct MsgOnDrop;
+
+impl Drop for MsgOnDrop {
+    fn drop(&mut self) {
+        rprintln!("Dropped `MsgOnDrop`!");
+    }
+}
+
+#[extendr]
+fn must_see_drop_msg_r_error() {
+    let _a = MsgOnDrop;
+
+    unsafe { Rf_error(c"%s".as_ptr(), c"threw an r error!".as_ptr()) }
+}
+
+#[extendr]
+fn must_see_drop_msg_panic() {
+    let _a = MsgOnDrop;
+
+    panic!()
+}
+
+extern "C" {
+    pub fn Rf_error(arg1: *const ::std::os::raw::c_char, ...) -> !;
+
+}
+
+extendr_module! {
+    mod leak;
+
+    fn must_see_drop_msg_r_error;
+    fn must_see_drop_msg_panic;
+}
