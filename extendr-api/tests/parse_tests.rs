9fcda0bdac3e1e137ca6aaeeafe9a387095597f8
CGMossa <cgmossa@gmail.com>
2025-11-26 17:27:12 +0100

    Refactor `parse` (#638)
    * DRAFT
    
    * DRAFT of a test
    
    * [skip ci]
    
    * codex finished this PR
    
    * [standalone] ci is adjusted
    
    * hmm fix
    
    * msrv fix
    
    * all tests require `-- --test-threads=1`.
    
    * ci: caching r dependencies even if failure occurs!

---
+++ extendr-api/tests/parse_tests.rs
@@ -0,0 +1,40 @@
+use extendr_api::prelude::*;
+use extendr_ffi::ParseStatus;
+
+#[test]
+fn parse_reports_error_for_invalid_syntax() {
+    test! {
+        let err = parse("c(10 42,20)").unwrap_err();
+        let msg = err.to_string();
+        match err {
+            Error::ParseError {
+                status: ParseStatus::PARSE_ERROR,
+                ..
+            } => assert!(msg.contains("syntax error")),
+            other => panic!("Unexpected error variant: {other:?}"),
+        }
+    }
+}
+
+#[test]
+fn parse_reports_error_for_incomplete_input() {
+    test! {
+        let err = parse("c(10").unwrap_err();
+        let msg = err.to_string();
+        match err {
+            Error::ParseError {
+                status: ParseStatus::PARSE_INCOMPLETE,
+                ..
+            } => assert!(msg.contains("incomplete statement")),
+            other => panic!("Unexpected error variant: {other:?}"),
+        }
+    }
+}
+
+#[test]
+fn eval_string_returns_eval_error_for_missing_argument() {
+    test! {
+        let result = eval_string("c(10,,42,20)");
+        assert!(matches!(result, Err(Error::EvalError(_))));
+    }
+}
