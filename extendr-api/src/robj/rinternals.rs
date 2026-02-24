4de5b8f496d98efa4378ba2584768959051be60e
Eli Daniels <90638398+eli-daniels@users.noreply.github.com>
2026-02-24 16:23:39 +0100

    chore: Rf_isFrame non-API (#1044)
    * chore: Rf_isFrame non-API
    
    * move backport from lib.rs to backports.rs
    
    * update readme and format
    
    ---------
    
    Co-authored-by: Josiah Parry <josiah.parry@gmail.com>
    (cherry picked from commit bd2377e9f2422e1750c7ca2e904bbeb6b1c91655)

--- extendr-api/src/robj/rinternals.rs
+++ extendr-api/src/robj/rinternals.rs
@@ -1,11 +1,11 @@
 use crate::*;
 use extendr_ffi::{
-    get_var, R_CFinalizer_t, R_ExternalPtrAddr, R_ExternalPtrProtected, R_ExternalPtrTag,
-    R_GetCurrentSrcref, R_GetSrcFilename, R_IsNamespaceEnv, R_IsPackageEnv, R_MakeExternalPtr,
-    R_MissingArg, R_NamespaceEnvSpec, R_PackageEnvName, R_RegisterCFinalizerEx, R_UnboundValue,
-    R_xlen_t, Rboolean, Rf_PairToVectorList, Rf_VectorToPairList, Rf_allocMatrix, Rf_allocVector,
-    Rf_asChar, Rf_asCharacterFactor, Rf_coerceVector, Rf_conformable, Rf_duplicate, Rf_findFun,
-    Rf_isArray, Rf_isComplex, Rf_isEnvironment, Rf_isExpression, Rf_isFactor, Rf_isFrame,
+    get_var, is_data_frame, R_CFinalizer_t, R_ExternalPtrAddr, R_ExternalPtrProtected,
+    R_ExternalPtrTag, R_GetCurrentSrcref, R_GetSrcFilename, R_IsNamespaceEnv, R_IsPackageEnv,
+    R_MakeExternalPtr, R_MissingArg, R_NamespaceEnvSpec, R_PackageEnvName, R_RegisterCFinalizerEx,
+    R_UnboundValue, R_xlen_t, Rboolean, Rf_PairToVectorList, Rf_VectorToPairList, Rf_allocMatrix,
+    Rf_allocVector, Rf_asChar, Rf_asCharacterFactor, Rf_coerceVector, Rf_conformable, Rf_duplicate,
+    Rf_findFun, Rf_isArray, Rf_isComplex, Rf_isEnvironment, Rf_isExpression, Rf_isFactor,
     Rf_isFunction, Rf_isInteger, Rf_isLanguage, Rf_isList, Rf_isLogical, Rf_isMatrix, Rf_isNewList,
     Rf_isNull, Rf_isNumber, Rf_isObject, Rf_isPrimitive, Rf_isReal, Rf_isS4, Rf_isString,
     Rf_isSymbol, Rf_isTs, Rf_isUserBinop, Rf_isVector, Rf_isVectorAtomic, Rf_isVectorList,
@@ -312,7 +312,7 @@ pub trait Rinternals: Types + Conversions {
 
     /// Return true if this is a data frame.
     fn is_frame(&self) -> bool {
-        unsafe { Rf_isFrame(self.get()).into() }
+        unsafe { is_data_frame(self.get()).into() }
     }
 
     /// Return true if this is a function or a primitive (CLOSXP, BUILTINSXP or SPECIALSXP)
--- extendr-ffi/README.md
+++ extendr-ffi/README.md
@@ -5,3 +5,49 @@
 ## Motivation
 
 extendr has historically relied on [libR-sys](https://github.com/extendr/libR-sys) to interface with R's C API. As R has moved towards standardizing and stabilizing the C API, relying on libR-sys' generated bindings has presented challenges. Among them are that the bindings are cumbersome to generate, platform specific, and does not provide backports to address the maturing R C API.
+
+## Creating backports
+
+As R's C API moves into stabilization, many macros and functions that we previously used are being moved to **non-API** status. This means the use of them will create a NOTE or WARNING during `R CMD check`. To address this, we create **backports**.
+
+To create a backport: 
+
+- Ensure there is a version `cfg` flag in `extendr-ffi/build.rs`
+- Move now non-API C binding to `src/backports.rs`'s `extern "C"` block
+- Add `#[cfg(not(r_maj_min))]` to old function
+- Add new replaced function with `#[cfg(r_maj_min)]`
+- Create a new inline function to be used in `extendr-api`
+- Replace old function calls in `extendr-api` with new backport
+
+For example, in R 4.5 `Rf_isFrame` is replaced by `Rf_isDataFrame`.
+In `backports.rs`
+
+```
+extern "C" {
+    // all the other backports...
+    
+    #[cfg(not(r_4_5))]
+    fn Rf_isFrame(x: SEXP) -> Rboolean;
+
+    #[cfg(r_4_5)]
+    fn Rf_isDataFrame(x: SEXP) -> Rboolean;
+}
+
+/// Check is data.frame
+///
+/// # Safety
+///
+/// This function dereferences a raw SEXP pointer.
+/// The caller must ensure that `x` is a valid SEXP pointer.
+#[inline]
+pub unsafe fn is_data_frame(x: SEXP) -> Rboolean {
+    #[cfg(not(r_4_5))]
+    {
+        Rf_isFrame(x)
+    }
+    #[cfg(r_4_5)]
+    {
+        Rf_isDataFrame(x)
+    }
+}
+```
