81dfc8ca35dddd1428a68c7c02ae8363943d44dc
Blake Vernon <kenneth.b.vernon@gmail.com>
2026-05-19 17:07:30 -0600

    remove make-wrappers.R

--- tests/extendrtests/R/make-wrappers.R
+++
@@ -1,8 +0,0 @@
-#' Generate wrapper code
-#' 
-#' Run `make_wrappers()` to update the wrapper scripts for this package.
-#' This autogenerates the file `R/extendr-wrappers.R`.
-#' @export
-make_wrappers <- function() {
-  rextendr::document()
-}
