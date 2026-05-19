9e14bbee35d1244ddec0c372a5179ff93baec753
Blake Vernon <kenneth.b.vernon@gmail.com>
2026-05-19 16:57:53 -0600

    call top-level write__ function

--- tests/extendrtests/src/rust/document.c
+++ tests/extendrtests/src/rust/document.c
@@ -1,20 +1,10 @@
-// Build-time helper: loaded transiently by R during package build to call
-// the wrapper-generation routine that `extendr_module!` already emits as
-// `wrap__make_<modname>_wrappers`. NOT compiled into the final package .so.
+// Build-time helper: linked against the package's Rust staticlib and run by
+// Makevars to write R/extendr-wrappers.R. NOT part of the final package .so.
 // See Makevars.in for how this is invoked.
 
-#include <R.h>
-#include <Rinternals.h>
-#include <R_ext/Rdynload.h>
+extern int write__make_extendrtests_wrappers(const char *package_name, const char *out_path);
 
-extern SEXP wrap__make_extendrtests_wrappers(SEXP use_symbols, SEXP package_name);
-
-static const R_CallMethodDef CallEntries[] = {
-    {"write_wrappers", (DL_FUNC) &wrap__make_extendrtests_wrappers, 2},
-    {NULL, NULL, 0}
-};
-
-void R_init_document(DllInfo *dll) {
-    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
+int main(int argc, char **argv) {
+    if (argc != 3) return 2;
+    return write__make_extendrtests_wrappers(argv[1], argv[2]);
 }
