f09c29e49fbf7bbc6b089cf49589aaab7d369995
JosiahParry <JosiahParry@users.noreply.github.com>
2026-06-30 17:10:35 +0000

    deploy: 183fe2d75177aead75f263b3f15e1455adb7ddf1

---
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["extendr_api",[["impl <a class=\"trait\" href=\"extendr_api/trait.FromStr.html\" title=\"trait extendr_api::FromStr\">FromStr</a> for <a class=\"struct\" href=\"extendr_api/wrapper/expr/struct.Expressions.html\" title=\"struct extendr_api::wrapper::expr::Expressions\">Expressions</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[291]}
